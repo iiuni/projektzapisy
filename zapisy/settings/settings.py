b9858a34f47a3a859bfc35a3564c88e0d64cd818
Rafał Łasocha <me+dev@swistak35.com>
2016-06-01 11:54:43 +0200

    Change PROJECT_PATH to valid one
    As we moved settings to subdirectory, PROJECT_PATH changed.

--- zapisy/settings/settings.py
+++ zapisy/settings/settings.py
@@ -3,7 +3,7 @@
 import os
 import logging
 
-PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 DEBUG = False
 
