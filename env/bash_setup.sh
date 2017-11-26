c3dcbce944a1773b74679e1edd4b180fec897246
ApamNapat <bartlomiejbetka@gmail.com>
2017-11-26 18:32:10 +0100

    Added some aliases

--- env/bash_setup.sh
+++ env/bash_setup.sh
@@ -1,7 +1,9 @@
 echo "alias prepare-env='source /home/vagrant/env2.6/bin/activate'" >> .bash_aliases
 echo "alias prepare-env27='source /home/vagrant/env2.7/bin/activate'" >> .bash_aliases
-echo "alias get-settings='cp /vagrant/env/settings_local.py .'" >> .bash_aliases
+echo "alias get-settings='cp /vagrant/env/settings_local.py /vagrant/zapisy/zapisy'" >> .bash_aliases
 echo "alias reset-database='sudo su - postgres -c \"psql -f reset_zapisy.sql\"'" >> .bash_aliases
 echo "alias load-database='sudo su - postgres -c \"psql -f /vagrant/a_dump_20160301.sql fereol\"'" >> .bash_aliases
 echo "alias runserver='python manage.py runserver 0.0.0.0:8000'" >> .bash_aliases
+echo "alias migrate='python /vagrant/zapisy/manage.py make_migrations && python /vagrant/zapisy/manage.py migrate'" >> .bash_aliases
+echo "alias development-setup='mkdir /vagrant/zapisy/logs && get-settings && sudo su - postgres -c \"psql -f /vagrant/db_dump.sql fereol\" && migrate'" >> .bash_aliases
 echo "source /home/vagrant/env2.7/bin/activate" >> .bashrc
