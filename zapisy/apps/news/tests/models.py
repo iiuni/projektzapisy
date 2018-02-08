073c662a11f046b8f4a97de4af6b6004a736ad17
ApamNapat <bartlomiejbetka@gmail.com>
2018-02-08 01:14:55 +0100

    Put the star import back

--- zapisy/apps/news/tests/models.py
+++ zapisy/apps/news/tests/models.py
@@ -8,19 +8,22 @@ from utils import generate_random_news
 from datetime import datetime, timedelta
 from random import randint
 
+
 class NewsModelTest(TestCase):
     def test_creating_news(self):
         n1 = News(title="news test",
                   body="news body")
         self.assertEquals(str(n1), "news test")
-    
+
+
 class NewsManagerTest(TestCase):
     def test_new(self):
         (new, old, ns) = generate_random_news('-')
+
         def f(o): return o.id
+
         nss = map(f, News.objects.new('-'))
-        ncs = map(f, sorted(ns,
-                key=(lambda o:o.date), reverse=True)[0:max(new,3)])
+        ncs = map(f, sorted(ns, key=(lambda o: o.date), reverse=True)[0:max(new, 3)])
         self.assertEquals(nss, ncs)
         for n in ns:
             n.delete()
@@ -36,45 +39,44 @@ class NewsManagerTest(TestCase):
         ns = []
         for i in xrange(12):
             n = News(title="news", body="news body", author_id=1,
-                     date=datetime.now() - timedelta(days=12-i))
+                     date=datetime.now() - timedelta(days=12 - i))
             n.save()
             ns.append(n)
-        self.assertEquals(0, len(News.objects.get_successive_news('-',0,0)))
-        self.assertEquals(0,len(
-            News.objects.get_successive_news('-',randint(0,11),0)))
+        self.assertEquals(0, len(News.objects.get_successive_news('-', 0, 0)))
+        self.assertEquals(0, len(
+            News.objects.get_successive_news('-', randint(0, 11), 0)))
         self.assertEquals(12, len(
-            News.objects.get_successive_news('-',0,20)))
-        nss = News.objects.get_successive_news('-',3, 6)
+            News.objects.get_successive_news('-', 0, 20)))
+        nss = News.objects.get_successive_news('-', 3, 6)
         for i in xrange(0, 6):
-            self.assertEquals(nss[i].id, ns[8-i].id)
+            self.assertEquals(nss[i].id, ns[8 - i].id)
         for n in ns:
             n.delete()
 
     def test_get_page_number_by_news_id(self):
-	(news, old, ns) = generate_random_news('1', 42, 0)
-	ids = [x.id for x in ns]
-	range_min, range_max = min(ids), max(ids)
-	self.assertEquals(News.objects.get_page_number_by_news_id(range_max), 1)
-	self.assertEquals(News.objects.get_page_number_by_news_id(range_max-14), 1)
-	self.assertEquals(News.objects.get_page_number_by_news_id(range_max-15), 2)
-	self.assertEquals(News.objects.get_page_number_by_news_id(range_min+20), 2)
-	self.assertEquals(News.objects.get_page_number_by_news_id(range_min+15), 2)
-	self.assertEquals(News.objects.get_page_number_by_news_id(range_min), 3)
-	self.assertEquals(News.objects.get_page_number_by_news_id(-1024), 1)
-	self.assertEquals(News.objects.get_page_number_by_news_id(2137), 1)
+        (news, old, ns) = generate_random_news('1', 42, 0)
+        ids = [x.id for x in ns]
+        range_min, range_max = min(ids), max(ids)
+        self.assertEquals(News.objects.get_page_number_by_news_id(range_max), 1)
+        self.assertEquals(News.objects.get_page_number_by_news_id(range_max - 14), 1)
+        self.assertEquals(News.objects.get_page_number_by_news_id(range_max - 15), 2)
+        self.assertEquals(News.objects.get_page_number_by_news_id(range_min + 20), 2)
+        self.assertEquals(News.objects.get_page_number_by_news_id(range_min + 15), 2)
+        self.assertEquals(News.objects.get_page_number_by_news_id(range_min), 3)
+        self.assertEquals(News.objects.get_page_number_by_news_id(-1024), 1)
+        self.assertEquals(News.objects.get_page_number_by_news_id(2137), 1)
 
-	for n in ns:
-	    n.delete()
-            
+        for n in ns:
+            n.delete()
+
         (news_unpublished, old_unpublished, ns) = generate_random_news('-', 50, 0)
-	ids = [x.id for x in ns]
-	range_min, range_max = min(ids), max(ids)
-	self.assertEquals(News.objects.get_page_number_by_news_id(range_max), 1)
-	self.assertEquals(News.objects.get_page_number_by_news_id(range_min), 1)
+        ids = [x.id for x in ns]
+        range_min, range_max = min(ids), max(ids)
+        self.assertEquals(News.objects.get_page_number_by_news_id(range_max), 1)
+        self.assertEquals(News.objects.get_page_number_by_news_id(range_min), 1)
 
-	for n in ns:
-	    n.delete()
-            
+        for n in ns:
+            n.delete()
 
     def test_count_new_categories(self):
         (new1, old1, ns1) = generate_random_news('1')
