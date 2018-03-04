97ba0a863fb8b983509359ef590285908d206f32
Kuba Kazimierczak <kuba@kazimierczak.name>
2018-03-04 21:41:00 +0100

    Fix polls test

--- zapisy/apps/grade/poll/models/poll.py
+++ zapisy/apps/grade/poll/models/poll.py
@@ -86,7 +86,7 @@ class Poll( models.Model ):
             if self.group:
                 if viewer == self.group.teacher: return True
 
-                lecture = next([x_y for x_y in GROUP_TYPE_CHOICES if x_y[1] == 'wykład'])[0]
+                lecture = [x_y for x_y in GROUP_TYPE_CHOICES if x_y[1] == 'wykład'][0][0]
                 groups  = Group.objects.filter( course = self.group.course,
                                                 teacher = viewer,
                                                 type    = lecture )
