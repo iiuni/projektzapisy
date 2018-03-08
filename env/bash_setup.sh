d810fc3206f22dfd235c6e0c1ed6a9695f8fa652
Kuba Kazimierczak <kuba@kazimierczak.name>
2018-03-08 09:03:30 +0100

    Remove useless bash aliases

--- env/bash_setup.sh
+++ env/bash_setup.sh
@@ -1,5 +1,2 @@
-echo "alias prepare-env27='source ~/env2.7/bin/activate'" >> .bash_aliases
-echo "alias prepare-env35='source ~/env3/bin/activate'" >> .bash_aliases
-echo "alias get-settings='cp /vagrant/env/settings_local.py .'" >> .bash_aliases
 echo "alias run='python /vagrant/zapisy/run.py'" >> .bash_aliases
 echo "source ~/env3/bin/activate" >> .bashrc
