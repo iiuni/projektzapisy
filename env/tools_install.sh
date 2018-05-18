08cc7de745d6a404bb95a1e84338f3177403d4b0
ApamNapat <bartlomiejbetka@gmail.com>
2018-05-18 20:27:40 +0200

    Made Vagrant setup more modular

---
+++ env/tools_install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+apt-get -y install git
+apt-get -y install libpq-dev
+apt-get -y install unzip
+apt-get -y install libxml2-dev libxslt1-dev
+apt-get -y install libc6-dev
+apt-get -y install libjpeg62-dev
+apt-get -y install libfreetype6-dev
+apt-get -y install liblcms1-dev
+apt-get -y install xvfb
+apt-get -y install firefox
+apt-get -y install memcached
