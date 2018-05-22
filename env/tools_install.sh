be6a29536eeb3362d817cdf4fc6b9976fa6367e6
ApamNapat <bartlomiejbetka@gmail.com>
2018-05-22 20:54:28 +0200

    Reverted previous changes, added explanatory comment to apt firefox install

--- env/tools_install.sh
+++ env/tools_install.sh
@@ -9,4 +9,5 @@ apt-get -y install libjpeg62-dev
 apt-get -y install libfreetype6-dev
 apt-get -y install liblcms1-dev
 apt-get -y install xvfb
+apt-get -y install firefox # This package is apparently installed to get the requirements for the firefox we get in firefox_and_geckodriver_setup.sh
 apt-get -y install memcached
