a5352abad2cbeb2ea0b2e93ddcf5e4f790ffd41e
Jasiek Marcinkowski <jasiekmarc@gmail.com>
2019-01-08 14:38:06 +0100

    Implements ProgramGroupRestrictions.
    A group can now be closed from enrolment for an entire studies program
    (major). This simplifies handling ISIM as well as new majors in future.

--- zapisy/apps/enrollment/records/models/opening_times.py
+++ zapisy/apps/enrollment/records/models/opening_times.py
@@ -12,7 +12,7 @@ from django.db import models, transaction
 
 from apps.enrollment.courses.models.semester import Semester
 from apps.enrollment.courses.models.group import Group
-from apps.users.models import Student
+from apps.users.models import Program, Student
 from apps.grade.ticket_create.models.student_graded import StudentGraded
 from apps.offer.vote.models.single_vote import SingleVote
 
@@ -96,14 +96,30 @@ class T0Times(models.Model):
             cls.objects.bulk_create(created)
 
 
+class ProgramGroupRestrictions(models.Model):
+    """Closes a group for the entire studies program.
+
+    This feature could potentially be implemented using GroupOpeningTimes but
+    having a simpler rule for that is neater. The rules saved in this model are
+    going to be used in methods of the GroupOpeningTimes class.
+    """
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    program = models.ForeignKey(Program, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("group", "program")
+        indexes = [
+            models.Index(fields=["group", "program"])
+        ]
+
+
 class GroupOpeningTimes(models.Model):
     """Stores student opening times for groups.
 
     The primary reason for a student to have this time specified is, that he
     voted for the course and gets a bonus (for all groups in that course).
-    Moreover we will use that to give special preferences to ISIM students and
-    to split groups into subgroups with different opening times during
-    freshmen enrollment.
+    Moreover we will use that to split groups into subgroups with different
+    opening times during freshmen enrollment.
     """
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
@@ -123,6 +139,12 @@ class GroupOpeningTimes(models.Model):
         there is none, the group might have its own opening and closing times.
         Finally, we look at the student's T0.
         """
+        try:
+            _ = ProgramGroupRestrictions.objects.get(program_id=student.program_id, group=group)
+            return False
+        except ProgramGroupRestrictions.DoesNotExist:
+            pass
+
         try:
             record = cls.objects.get(student=student, group=group)
             return record.time <= time <= group.course.semester.records_closing
@@ -151,11 +173,18 @@ class GroupOpeningTimes(models.Model):
 
         for k in groups:
             groups[k].opening_time_for_student = None
+            groups[k].restricted_for_student = False
         for rec in cls.objects.filter(student=student, group__in=groups):
             groups[rec.group_id].opening_time_for_student = rec.time
+        for rec in ProgramGroupRestrictions.objects.filter(
+                program_id=student.program_id, group__in=groups):
+            groups[rec.group_id].restricted_for_student = True
 
         ret: Dict[int, bool] = {}
         for k, group in groups.items():
+            if group.restricted_for_student:
+                ret[k] = False
+                continue
             if group.opening_time_for_student is not None:
                 ret[k] = (
                     group.opening_time_for_student <= time <= group.course.semester.records_closing
