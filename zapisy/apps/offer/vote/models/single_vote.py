b0193ad7452d8e5cbbebe69204549f79da912efb
Jasiek Marcinkowski <jasiekmarc@gmail.com>
2019-04-13 15:53:53 +0200

    Ostre czyszczenie w aplikacji vote.

--- zapisy/apps/offer/vote/models/single_vote.py
+++ zapisy/apps/offer/vote/models/single_vote.py
@@ -1,230 +1,44 @@
-"""
-    Single vote
-"""
-
 from datetime import date
+
 from django.core.exceptions import ObjectDoesNotExist
-from django.db import models, DatabaseError, transaction
+from django.db import DatabaseError, models, transaction
 from django.db.models.aggregates import Sum
-from apps.enrollment.courses.models.course import CourseEntity, Course
+
+from apps.enrollment.courses.models.course import Course, CourseEntity
 from apps.enrollment.courses.models.semester import Semester
-from apps.offer.vote.models.system_state import SystemState
+from apps.offer.proposal.models import Proposal, ProposalStatus
+from apps.users.models import Student
+
+from .system_state import SystemState
 
 
 class SingleVote (models.Model):
-    """
-        Student's single vote
+    """Student's single vote for a course proposal in an academic cycle (year).
     """
     votes = [(0, '0'), (1, '1'), (2, '2'), (3, '3')]
 
-    student = models.ForeignKey('users.Student', on_delete=models.CASCADE, verbose_name='głosujący')
-
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, verbose_name="głosujący")
     entity = models.ForeignKey(CourseEntity, verbose_name='podstawa', on_delete=models.CASCADE)
+    proposal = models.ForeignKey(Proposal, verbose_name="propozycja")
 
-    course = models.ForeignKey(Course, null=True, blank=True, on_delete=models.SET_NULL)
+    state = models.ForeignKey(SystemState, on_delete=models.CASCADE, verbose_name="Rok akademicki")
 
-    state = models.ForeignKey(
-        'vote.SystemState',
-        on_delete=models.CASCADE,
-        verbose_name='ustawienia głosowania')
+    value = models.SmallPositiveIntegerField("przyznane punkty", choices=votes, default=0)
+    correction = models.SmallPositiveIntegerField(
+        "punkty przyznane w korekcie", choices=votes, default=0)
 
-    value = models.IntegerField(choices=votes, default=0, verbose_name='punkty')
-    correction = models.IntegerField(choices=votes, default=0, verbose_name='korekta')
-
-    free_vote = models.BooleanField(default=False, verbose_name='Głos nie liczy się do limitu')
-    # powyższe pole służy do odróżnienia np glosow dopisanych, ktorych nie chcemy liczyc do limitu
+    # The field used to count votes that should not count into the limit.
+    free_vote = models.BooleanField(default=False, verbose_name="Głos nie liczy się do limitu")
 
     class Meta:
-        verbose_name = 'pojedynczy głos'
-        verbose_name_plural = 'pojedyncze głosy'
+        verbose_name = "pojedynczy głos"
+        verbose_name_plural = "pojedyncze głosy"
         app_label = 'vote'
         ordering = ('student', 'entity', '-value')
 
         unique_together = ('course', 'state', 'student')
 
     def __str__(self):
-        return '[' + str(self.state.year) + ']Głos użytkownika: ' + \
-            self.student.user.username + '; ' + self.entity.name + \
-            '; ' + str(self.value)
+        return (f"[{self.state.year}] Głos użytkownika: {self.student.user.username}; "
+                f"{self.proposal.name}; {self.value}")
 
-    @staticmethod
-    def clean_votes():
-        votes = SingleVote.objects.all()
-        a = {}
-        for v in votes:
-            if str(v.student_id) in a:
-                if str(v.entity_id) in a[str(v.student_id)]:
-                    a[str(v.student_id)][str(v.entity_id)].value = max(
-                        a[str(v.student_id)][str(v.entity_id)].value, v.value)
-                    a[str(v.student_id)][str(v.entity_id)].save()
-                    v.delete()
-                else:
-                    a[str(v.student_id)][str(v.entity_id)] = v
-
-            if str(v.student_id) not in a:
-                a[str(v.student_id)] = {}
-
-    @staticmethod
-    def get_votes(voter, year=None, state=None,):
-        """
-            Gets user votes in specified year
-        """
-        if not state:
-            if not year:
-                year = date.today().year
-            state = SystemState.get_state(year)
-
-        return SingleVote.objects.filter(
-            student=voter,
-            state=state,
-            correction__gte=1) .select_related(
-            'student',
-            'student__user',
-            'entity').order_by(
-            'entity__semester',
-            'entity__name')
-
-    @staticmethod
-    def add_vote_count(proposals, state):
-        return proposals.extra(
-            select={
-                'votes': "COALESCE((SELECT SUM(vote_singlevote.correction) FROM vote_singlevote WHERE vote_singlevote.entity_id = courses_courseentity.id AND vote_singlevote.state_id = %d), 0)" % state.id,
-                'voters': "SELECT COUNT(*) FROM vote_singlevote WHERE vote_singlevote.entity_id = courses_courseentity.id AND vote_singlevote.correction > 0 AND vote_singlevote.state_id = %d" % state.id,
-            },
-        )
-
-    @staticmethod
-    def get_points_and_voters(proposal, year=None, state=None):
-        """
-            Gets proposal points and voters count in specified year
-        """
-        if not state:
-            if not year:
-                year = date.today().year
-            current_state = SystemState.get_state(year)
-        else:
-            current_state = state
-        votes = SingleVote.objects.filter(entity=proposal, state=current_state, correction__gte=1)\
-            .select_related('student', 'student__user', 'entity')
-
-        value = 0
-        voters = 0
-        for vote in votes:
-            value += vote.correction
-            voters += 1
-
-        return value, voters, votes
-
-    @staticmethod
-    def make_votes(student, year=None, state=None, tag='summer'):
-        """
-            Makes 'zero' vote for student - only for proposal without
-            vote
-        """
-        correction = state.is_correction_active()
-
-        semester = None
-        if tag == 'winter':
-            semester = state.semester_winter
-        if tag == 'summer':
-            semester = state.semester_summer
-
-        year = year if year else date.today().year
-
-        proposals = CourseEntity.objects.filter(status=CourseEntity.STATUS_TO_VOTE, deleted=False)
-        current_state = SystemState.get_state(year)
-
-        old_votes = SingleVote.objects.\
-            filter(student=student, state=current_state).\
-            values_list('entity__id', flat=True).order_by('entity__id')
-
-        new_votes = []
-        for proposal in proposals:
-            if proposal.id not in old_votes:
-                kwargs = {'student': student, 'entity': proposal, 'state': current_state}
-                if correction:
-                    try:
-                        kwargs['course'] = Course.objects.get(semester=semester, entity=proposal)
-                    except ObjectDoesNotExist:
-                        pass
-                new_votes.append(SingleVote(**kwargs))
-
-        if new_votes:
-            SingleVote.objects.bulk_create(new_votes)
-
-    @staticmethod
-    def get_votes_for_proposal(voter, proposals, year=None):
-        #        """
-        #            Gets user votes in specified year for proposal set
-        #        """
-        if not year:
-            year = date.today().year
-        current_state = SystemState.get_state(year)
-
-        return SingleVote.objects.filter(student=voter, state=current_state, entity__in=proposals)\
-            .select_related('entity',
-                            'entity__owner',
-                            'entity__owner__user',
-                            'entity__type')
-
-    @staticmethod
-    def sum_votes(student, state):
-        return SingleVote.objects.filter(
-            student=student,
-            state=state,
-            entity__type__free_in_vote=False).aggregate(
-            votes=Sum('value'))
-
-    @staticmethod
-    def sum_old_votes(student, state):
-        return SingleVote.objects.filter(
-            student=student,
-            state=state,
-            entity__type__free_in_vote=False) .extra(
-            where=[
-                '(SELECT COUNT(*) FROM courses_course cc WHERE cc.entity_id = vote_singlevote.entity_id AND cc.semester_id = ' +
-                str(
-                    state.semester_summer_id) +
-                ') = 0']) .aggregate(
-            votes=Sum('correction'))
-
-    @staticmethod
-    def limit_in_summer_correction(student, state):
-        all = SingleVote.objects.filter(
-            student=student, state=state, entity__type__free_in_vote=False) .extra(
-            where=["vote_singlevote.entity_id IN (SELECT id FROM courses_courseentity WHERE semester = 'l')"]) .aggregate(
-            votes=Sum('value'))
-
-        return all['votes']
-
-    @staticmethod
-    def limit_in_winter_correction(student, state):
-        all = SingleVote.objects.filter(
-            student=student, state=state, entity__type__free_in_vote=False) .extra(
-            where=["vote_singlevote.entity_id IN (SELECT id FROM courses_courseentity WHERE semester = 'z')"]) .aggregate(
-            votes=Sum('value'))
-
-        return all['votes']
-
-    def get_vote(self):
-        return self.correction
-
-    @classmethod
-    def populate_scheduled_courses(cls, semester: Semester):
-        """Populates course filed in all singlevotes for a given semester/year.
-
-        When the schedule for semester is known, Course instances are created in
-        the database for CourseEntities. These course instances can be then
-        referred to in SingleVote object course field. This will make data more
-        consistent and opening times easier to compute.
-        """
-        with transaction.atomic():
-            cls.objects.filter(value=0, correction=0).delete()
-            course: Course
-            for course in Course.objects.filter(semester=semester):
-                cls.objects.filter(
-                    models.Q(state__semester_winter_id=semester.id) |
-                    models.Q(state__semester_summer_id=semester.id),
-                    entity_id=course.entity_id,
-                    course__isnull=True
-                ).update(course=course)
--- zapisy/apps/offer/vote/models/system_state.py
+++ zapisy/apps/offer/vote/models/system_state.py
@@ -1,77 +1,66 @@
-"""
-    System State for vote
-    Default values are dafined as module variables
-"""
-from django.core.exceptions import ObjectDoesNotExist
+"""System State for vote application.
 
-from django.db import models
+Voting cycle spans two semesters in one academic year. Thus the system state
+corresponds to a single academic year.
+"""
 from datetime import date
+import re
+from typing import Optional
+
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
+from django.db import models
+
 from apps.enrollment.courses.models.semester import Semester
 
-DEFAULT_YEAR = date.today().year - 2
 DEFAULT_MAX_POINTS = 50
 DEFAULT_MAX_VOTE = 3
-DEFAULT_DAY_BEG = 1          #
-DEFAULT_DAY_END = 31         # Te dane trzeba będzie tak ustawić
-DEFAULT_MONTH_BEG = 1          # żeby były prawdziwe. Na razie tak
-DEFAULT_MONTH_END = 7         # jest wygodnie, chociażby do testów
-DEFAULT_VOTE_BEG = date(DEFAULT_YEAR, 6, 10)
-DEFAULT_VOTE_END = date(DEFAULT_YEAR, 7, 10)
-DEFAULT_CORRECTION_BEG = date(DEFAULT_YEAR, DEFAULT_MONTH_BEG, DEFAULT_DAY_BEG)
-DEFAULT_CORRECTION_END = date(DEFAULT_YEAR, DEFAULT_MONTH_END, DEFAULT_DAY_END)
 
 
 class SystemState(models.Model):
-    """
-        System state for vote
-    """
 
     semester_winter = models.ForeignKey(Semester, on_delete=models.CASCADE,
-                                        verbose_name='Semestr zimowy',
+                                        verbose_name="Semestr zimowy",
                                         related_name='winter_votes',
                                         null=True, blank=True)
 
     semester_summer = models.ForeignKey(Semester,
                                         on_delete=models.CASCADE,
-                                        verbose_name='Semestr letni',
+                                        verbose_name="Semestr letni",
                                         related_name='summer_votes',
                                         null=True, blank=True)
 
-    year = models.IntegerField(
-        verbose_name='Rok akademicki',
-        default=date.today().year)
+    def get_default_year():
+        """Usually we are creating a new system state for the upcoming year."""
+        current_year = date.today().year
+        return f"{current_year}/{current_year % 100 + 1}"
 
-    max_points = models.IntegerField(
-        verbose_name='Maksimum punktów na przedmioty',
-        default=DEFAULT_MAX_POINTS)
+    def validate_year_format(value: str):
+        """Verifies that the year is in format YYYY/YY."""
+        m = re.fullmatch('(\d{4})/(\d{2})', value)
+        if not m:
+            raise ValidationError(
+                "%(value)s does not comply to format YYYY/YY.", params={'value', value})
+        y1 = int(m.group(1)) % 100
+        y2 = int(m.group(2))
+        if y1 + 1 != y2:
+            raise ValidationError("Academic year should span two calendar years.")
 
-    max_vote = models.IntegerField(
-        verbose_name='Maksymalna wartość głosu',
-        default=DEFAULT_MAX_VOTE)
 
-    vote_beg = models.DateField(
-        verbose_name='Początek głosowania',
-        default=DEFAULT_VOTE_BEG)
+    year = models.CharField("Rok akademicki", max_length=7,
+        default=get_default_year, validators=[validate_year_format])
 
-    vote_end = models.DateField(
-        verbose_name='Koniec głosowania',
-        default=DEFAULT_VOTE_END)
+    vote_beg = models.DateField("Początek głosowania", null=True, default=None)
+    vote_end = models.DateField("Koniec głosowania", null=True, default=None)
 
     winter_correction_beg = models.DateField(
-        verbose_name='Początek korekty zimowej',
-        default=DEFAULT_CORRECTION_BEG)
-
+        "Początek korekty na semestr zimowy", null=True, default=None)
     winter_correction_end = models.DateField(
-        verbose_name='Koniec korekty zimowej',
-        default=DEFAULT_CORRECTION_END)
+        "Koniec korekty na semestr zimowy", null=True, default=None)
 
     summer_correction_beg = models.DateField(
-        verbose_name='Początek korekty letniej',
-        default=DEFAULT_CORRECTION_BEG)
-
+        "Początek korekty na semestr letni", null=True, default=None)
     summer_correction_end = models.DateField(
-        verbose_name='Koniec korekty letniej',
-        default=DEFAULT_CORRECTION_END)
+        "Koniec korekty na semestr letni", null=True, default=True)
 
     class Meta:
         verbose_name = 'ustawienia głosowania'
@@ -79,60 +68,24 @@ class SystemState(models.Model):
         app_label = 'vote'
 
     def __str__(self):
-        return "Ustawienia systemu na rok " + str(self.year)
+        return f"Ustawienia systemu na rok akademicki {self.year}"
 
     @staticmethod
-    def get_state(year=None):
+    def get_current_state() -> Optional['SystemState']:
+        """Returns the state corresponding to the current semester.
+        
+        If one does not exist, returns None.
         """
-            Gets actual system state from database
-            Creates one if necessary
-        """
-        if not year:
-            year = date.today().year
-        try:
-            return SystemState.objects.get(year=year)
-        except ObjectDoesNotExist:
-            return SystemState.create_default_state(year)
-
-    @staticmethod
-    def create_default_state(year=None):
-        """
-            Creates system state from default variables
-        """
-        if not year:
-            year = date.today().year
-        new_state = SystemState()
-        new_state.year = year
-        new_state.max_points = DEFAULT_MAX_POINTS
-        new_state.vote_beg = date(year, 6, 10)
-        new_state.vote_end = date(year, 7, 10)
-        new_state.save()
-        return new_state
-
-    def is_system_active(self):
-        return self.is_vote_active() or self.is_correction_active()
-
-    def is_vote_active(self):
-        """
-            Checks if vote is active
-        """
-        today = date.today()
-
-        return self.vote_beg <= today <= self.vote_end
-
-    def is_correction_active(self):
-        """
-            Checks if correction is active
-        """
-
-        return self.is_winter_correction_active() or self.is_summer_correction_active()
-
-    def is_winter_correction_active(self):
-        today = date.today()
-
-        return self.winter_correction_beg <= today <= self.winter_correction_end
-
-    def is_summer_correction_active(self):
-        today = date.today()
-
-        return self.summer_correction_beg <= today <= self.summer_correction_end
+        state: Optional[SystemState] = None
+        s = Semester.objects.get_next()
+        if s.type == Semester.TYPE_WINTER:
+            try:
+                state = SystemState.objects.get(semester_winter=s)
+            except SystemState.DoesNotExist:
+                return None
+        if s.type == Semester.TYPE_SUMMER:
+            try:
+                state = SystemState.objects.get(semester_summer=s)
+            except SystemState.DoesNotExist:
+                return None
+        return state
