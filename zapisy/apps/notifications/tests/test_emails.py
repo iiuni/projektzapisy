529a72f6f95d90084083278f93d3300608d6071c
barnij <barnij@windowslive.com>
2019-05-11 00:07:13 +0200

    Naprawa testu test_emails

--- zapisy/apps/notifications/tests/test_emails.py
+++ zapisy/apps/notifications/tests/test_emails.py
@@ -3,7 +3,7 @@ from django.test import TestCase, override_settings
 from django.template.loader import render_to_string
 
 from apps.enrollment.courses.tests.factories import GroupFactory, CourseFactory
-from apps.users.tests.factories import StudentFactory
+from apps.users.tests.factories import EmployeeFactory
 from apps.notifications.templates import NotificationType
 from apps.notifications.utils import render_description
 from apps.notifications.custom_signals import teacher_changed
@@ -14,23 +14,21 @@ from apps.enrollment.courses.models.group import Group
 @override_settings(RUN_ASYNC=False)
 class NotificationsEmailTestCase(TestCase):
     def test_teacher_changed(self):
-        student = StudentFactory()
+        teacher = EmployeeFactory()
         course = CourseFactory()
         group = GroupFactory(course=course)
         mail.outbox = []
 
-        teacher_changed.send(sender=Group, instance=group, user=student.user)
+        teacher_changed.send(sender=Group, instance=group)
 
         ctx = {
             'content':
             render_description(
-                NotificationType.TEACHER_HAS_BEEN_CHANGED, {
-                    "course_name": group.course.information.entity.name,
-                    "teacher": group.teacher.user.get_full_name(),
-                    "type": group.human_readable_type().lower()
+                NotificationType.ASSIGNED_TO_NEW_GROUP_AS_A_TEACHER, {
+                    "course_name": group.course.information.entity.name
                 }),
             'greeting':
-            f'Dzień dobry, {student.user.first_name}',
+            f'Dzień dobry, {teacher.user.first_name}',
         }
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].body, render_to_string('notifications/email_base.html', ctx))
