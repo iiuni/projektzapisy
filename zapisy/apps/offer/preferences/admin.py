baf503565e8b222f856cf8b62502effff6234fab
Jan Marcinkowski <jasiekmarc@gmail.com>
2019-05-11 23:46:10 +0200

    Admin dla aplikacji Preferences.

--- zapisy/apps/offer/preferences/admin.py
+++ zapisy/apps/offer/preferences/admin.py
@@ -1,10 +1,22 @@
-"""
-    Preferences admin
-"""
-
 from django.contrib import admin
 
 from .models import PreferencesQuestion, Preference
 
-admin.site.register(PreferencesQuestion)
-admin.site.register(Preference)
+
+@admin.register(PreferencesQuestion)
+class PreferencesQuestionAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['proposal']
+
+
+@admin.register(Preference)
+class PreferenceAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'proposal', 'class_type', 'answer')
+    list_filter = (('employee', admin.RelatedOnlyFieldListFilter),
+                   ('question__proposal',
+                    admin.RelatedOnlyFieldListFilter), 'question__class_type', 'answer')
+
+    def proposal(self, obj):
+        return obj.question.proposal
+
+    def class_type(self, obj):
+        return obj.question.get_class_type_display()
