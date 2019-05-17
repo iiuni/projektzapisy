c5d97b9d9d6893116a8d87cfaaaac8e47b20bc17
Mateusz Lewandowski <mlewandowski@cs.uni.wroc.pl>
2019-05-17 14:08:12 +0200

    removed settings related to issue tracker; modified templates with github tracker link

--- zapisy/zapisy/settings.py
+++ zapisy/zapisy/settings.py
@@ -351,12 +351,6 @@ CACHES = {
 
 NEWS_PER_PAGE = 15
 
-# The URL to the issue tracker where users
-# can submit issues or bug reports. Used in several templates.
-ISSUE_TRACKER_URL = "https://github.com/iiuni/projektzapisy/issues"
-# As above, but takes the user straight to the "create new issue" page
-ISSUE_TRACKER_NEW_ISSUE_URL = "https://github.com/iiuni/projektzapisy/issues/new"
-
 if os.path.isfile(os.path.join(BASE_DIR, 'zapisy', 'pipeline.py')):
     exec(open(os.path.join(BASE_DIR, 'zapisy', 'pipeline.py')).read())
 
