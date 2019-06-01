ba7d909108a7ac4aee17a84325a31d56d7ce9ae5
barnij <barnij@windowslive.com>
2019-06-02 01:39:33 +0200

    Powiadomienie o zmianie prowadzącego grupy, Wyświetlanie powiadomień w kolejności wysłania

--- zapisy/apps/notifications/models.py
+++ zapisy/apps/notifications/models.py
@@ -2,15 +2,34 @@ from typing import List
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Q
 
 from apps.enrollment.courses.models.group import Group
-from apps.enrollment.records.models import Record
+from apps.enrollment.records.models import Record, RecordStatus
 from apps.users.models import Student
 
 
 def get_all_users_in_course_groups(course_groups: List[Group]):
     records = Record.objects.filter(group__in=course_groups, status=1).select_related(
         'student', 'student__user')
+
+    return {element.student.user for element in records}
+
+
+def get_all_users_from_group(group: Group):
+    records = Record.objects.filter(Q(group=group) & (Q(status=RecordStatus.QUEUED) | Q(status=RecordStatus.ENROLLED))).select_related('student', 'student__user')
+
+    return {element.student.user for element in records}
+
+
+def get_queued_users_from_group(group: Group):
+    records = Record.objects.filter(group=group, status=RecordStatus.QUEUED).select_related('student', 'student__user')
+
+    return {element.student.user for element in records}
+
+
+def get_enrolled_users_from_group(group: Group):
+    records = Record.objects.filter(group=group, status=RecordStatus.ENROLLED).select_related('student', 'student__user')
 
     return {element.student.user for element in records}
 
@@ -33,8 +52,10 @@ class NotificationPreferencesStudent(models.Model):
     #not_pulled_from_queue = models.BooleanField(default=True, verbose_name='Anulowanie wciągnięcia do grupy')
     added_new_group = models.BooleanField(default=True, verbose_name='Dodanie nowej grupy przedmiotu, na który jesteś '
                                                                      'zapisany')
-    teacher_has_been_changed = models.BooleanField(default=True, verbose_name='Zmiana prowadzącego grupy z przedmiotu, '
-                                                                              'na który jesteś zapisany')
+    teacher_has_been_changed_enrolled = models.BooleanField(default=True, verbose_name='Zmiana prowadzącego grupy z przedmiotu, '
+                                                                                       'na który jesteś zapisany/a')
+    teacher_has_been_changed_queued = models.BooleanField(default=True, verbose_name='Zmiana prowadzącego grupy z przedmiotu, '
+                                                                                     'na który czekasz w kolejce')
     news_has_been_added = models.BooleanField(default=True, verbose_name='Powiadomienie o nowej wiadomości w Aktualnościach')
 
 
