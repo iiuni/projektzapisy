6a8c8e9c872044fa492a98ada9742bbb3f271aee
Clechay <luke.clechay@gmail.com>
2019-06-09 20:24:33 +0200

    Allow for desecting owner

--- zapisy/apps/enrollment/timetable/assets/components/CourseFilterOwner.vue
+++ zapisy/apps/enrollment/timetable/assets/components/CourseFilterOwner.vue
@@ -2,21 +2,25 @@
 import Vue from "vue";
 import { mapGetters } from "vuex";
 import Component from "vue-class-component";
-
 import { CourseShell, Group, Course } from "../models";
+
+const cancelSelectionText = "-nie wybieraj-";
 
 @Component({
   props: {
 		allAvaiable: Array as ()=>string[],
 		owner: String as ()=>string,
-	},
+  },
 })
 export default class CourseFilterOwner extends Vue {
-  // The computed property selectionState comes from store.
+  get allFinallyAvaiable(): string[]{
+    return [cancelSelectionText, ...this.$props.allAvaiable]
+  }
   get selection(): string {
 		return this.$props.owner;
   }
   set selection(value: string) {
+    if(value === cancelSelectionText) value="";
     this.$store.dispatch("filters/setFilter", ["owner",value]);
   }
 }
@@ -26,7 +30,7 @@ export default class CourseFilterOwner extends Vue {
 	<div class="form-group">
 		<h4>Organizator przedmiotu</h4>
     <select class="form-control" v-model="selection" id="exampleFormControlSelect2">
-      <option v-bind:key="avaiable" v-for="avaiable in allAvaiable">{{avaiable}}</option>
+      <option v-bind:key="avaiable" v-for="avaiable in allFinallyAvaiable">{{avaiable}}</option>
     </select>
   </div>
 </template>
