7f1eae3cc7ce24d06f2c8891cc3031c3835282dd
Jan Marcinkowski <jasiekmarc@gmail.com>
2019-10-12 01:23:09 +0200

    Przeniesienie ikon do komponenty.

--- zapisy/apps/offer/proposal/assets/MarkdownEditor.vue
+++ zapisy/apps/offer/proposal/assets/MarkdownEditor.vue
@@ -1,96 +1,108 @@
 <template>
-  <div>
-    <div class="md-editor-wrapper">
-      <textarea
-          :class="{'is-invalid': is_invalid}"
-          rows="10" :value="input" :name="name"
-          @input="update"></textarea>
-      <div class="preview">
-        <span v-html="compiledMarkdown"></span>
-        <a
-         class="doc-link"
-         href="https://guides.github.com/features/mastering-markdown/#examples"
-         target="_blank">
-          <font-awesome-icon :icon="['fab', 'markdown']" />
-        </a>
-      </div>
+    <div>
+        <div class="md-editor-wrapper">
+            <textarea
+                :class="{ 'is-invalid': is_invalid }"
+                rows="10"
+                :value="input"
+                :name="name"
+                @input="update"
+            ></textarea>
+            <div class="preview">
+                <span v-html="compiledMarkdown"></span>
+                <a
+                    class="doc-link"
+                    href="https://guides.github.com/features/mastering-markdown/#examples"
+                    target="_blank"
+                >
+                    <font-awesome-icon :icon="faMarkdown" />
+                </a>
+            </div>
+        </div>
+        <slot></slot>
     </div>
-    <slot></slot>
-  </div>
 </template>
 
 <script>
+import { faMarkdown } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import marked from "marked";
 import { debounce, uniqueId } from "lodash";
 
 export default {
-  props: {
-    name: String,
-    value: {
-      type: String,
-      required: false,
+    props: {
+        name: String,
+        value: {
+            type: String,
+            required: false
+        },
+        is_invalid: {
+            type: Boolean,
+            default: false,
+            required: false
+        }
     },
-    is_invalid: {
-      type: Boolean,
-      default: false,
-      required: false,
-    }
-  },
-  data: function() {
-    return {
-      input: this.value ? this.value : "",
-    };
-  },
-  computed: {
-    compiledMarkdown: function() {
-      return marked(this.input, { sanitize: true });
-    }
-  },
-  methods: {
-    update: debounce(function(e) {
-      this.input = e.target.value;
-    }, 300)
-  }
+    data: function() {
+        return {
+            input: this.value ? this.value : "",
+            faMarkdown: faMarkdown,
+        };
+    },
+    computed: {
+        compiledMarkdown: function() {
+            return marked(this.input, { sanitize: true });
+        }
+    },
+    methods: {
+        update: debounce(function(e) {
+            this.input = e.target.value;
+        }, 300)
+    },
+
+    components: {
+      FontAwesomeIcon,
+    },
 };
 </script>
 
 <style lang="scss" scoped>
 .md-editor-wrapper {
-  display: flex;
-  align-items: stretch;
-  justify-content: space-between;
+    display: flex;
+    align-items: stretch;
+    justify-content: space-between;
 
-  @media (max-width: 767px) {
-    flex-direction: column;
-  }
-
-  border: 1px solid #ced4da;
-  border-radius: 0.25rem;
-
-  textarea {
-    border: none;
-    background: #f8f9fa;
-    font-family: SFMono-Regular,Menlo,Monaco,Consolas,Liberation Mono,Courier New,monospace;
-
-    @media (min-width: 768px) {
-      width: calc(50% - .25em);
+    @media (max-width: 767px) {
+        flex-direction: column;
     }
-    padding: 1em;
-  }
 
-  .preview {
-    @media (min-width: 768px) {
-      width: calc(50% - .25em);
-    }
-    position: relative;
-    padding: 1em;
+    border: 1px solid #ced4da;
+    border-radius: 0.25rem;
 
-    .doc-link {
-      color: black;
-      position: absolute;
-      bottom: .25em;
-      right: .5em;
+    textarea {
+        border: none;
+        background: #f8f9fa;
+        font-family: SFMono-Regular, Menlo, Monaco, Consolas, Liberation Mono,
+            Courier New, monospace;
+
+        @media (min-width: 768px) {
+            width: calc(50% - 0.25em);
+        }
+        padding: 1em;
     }
-  }
+
+    .preview {
+        @media (min-width: 768px) {
+            width: calc(50% - 0.25em);
+        }
+        position: relative;
+        padding: 1em;
+
+        .doc-link {
+            color: black;
+            position: absolute;
+            bottom: 0.25em;
+            right: 0.5em;
+        }
+    }
 }
 </style>
--- zapisy/apps/offer/proposal/assets/markdown-editor.js
+++ zapisy/apps/offer/proposal/assets/markdown-editor.js
@@ -1,14 +1,6 @@
 import Vue from "vue/dist/vue.js";
 import MarkdownEditor from "./MarkdownEditor.vue";
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faMarkdown } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-library.add(faMarkdown);
-
-Vue.component("font-awesome-icon", FontAwesomeIcon);
-
 new Vue({
     el: "#edit-proposal-form",
     components: {
