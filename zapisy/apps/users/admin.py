f0278a361387d7726d906e4119e81fb0a0ac8feb
adam91091 <adam.kufel2@gmail.com>
2019-10-20 10:50:40 +0200

    Remove fields from BaseUser class: - receive_mass_mail_enrollment - receive_mass_mail_offer - receive_mass_mail_grade Remove dependencies in poll_test.json and admin.py Add new migration applying above changes in db

--- zapisy/apps/users/admin.py
+++ zapisy/apps/users/admin.py
@@ -64,7 +64,7 @@ class StudentAdmin(admin.ModelAdmin):
         (None, {'fields': ['user', 'matricula', 'status']}),
         ('Studia', {'fields': ['numeryczna_l', 'dyskretna_l', 'program', 'semestr', 'ects']}),
         ('Zapisy', {'fields': ['records_opening_bonus_minutes', 'block']}),
-        ('Inne', {'fields': ['receive_mass_mail_enrollment', 'receive_mass_mail_offer', 'receive_mass_mail_grade', 'last_news_view'], 'classes': ['collapse']}),
+        ('Inne', {'fields': ['last_news_view'], 'classes': ['collapse']}),
     ]
     search_fields = ('user__first_name', 'user__last_name', 'matricula')
     list_filter = ('program', 'status', 'semestr')
@@ -87,8 +87,7 @@ class EmployeeAdmin(admin.ModelAdmin):
         (None, {
             'fields': [
                 'user', 'status', 'homepage', 'room', 'consultations']}), ('Ogłoszenia mailowe', {
-                    'fields': [
-                        'receive_mass_mail_enrollment', 'receive_mass_mail_offer'], 'classes': ['collapse']}), ]
+                    'fields': [], 'classes': ['collapse']}), ]
     ordering = ['user__last_name', 'user__first_name']
     list_display_links = ('get_full_name',)
 
