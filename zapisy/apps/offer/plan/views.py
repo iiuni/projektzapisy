d99673def618fbbf77c0e8b9b766bcd4ba182629
TheGroch <zydolinho@gmail.com>
2019-12-06 15:45:51 +0100

    Dodanie funkcji propose, drobne zmiany w views.py

--- zapisy/apps/offer/plan/views.py
+++ zapisy/apps/offer/plan/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from apps.users.models import BaseUser
 from django.urls import reverse
+from apps.offer.vote.models.system_state import SystemState
 
-from apps.offer.plan.utils import get_votes
+from apps.offer.plan.utils import get_votes, propose
 
 
 def plan_view(request):
@@ -14,15 +15,15 @@ def plan_view(request):
 
 def plan_create(request):
     if request.user.is_superuser:
-        courses_proposal = get_votes(1)
+        courses_proposal = get_votes(3)
         courses = []
+        current_year = SystemState.get_current_state().year
         for key, value in courses_proposal.items():
-            for k, v in value.items():
-                # First value is the name of course
-                # Second value is the semester when the course is planned to be
-                # Third value says if this course is proposed
-                courses.append([key, v['semester'], False])
-
+            # First value is the name of course
+            # Second value is the semester when the course is planned to be
+            # Third value says if this course is proposed
+            courses.append([key, value[current_year]['semester'], propose(value)])
+                
         context = {'courses_proposal': courses}
         return render(request, 'plan/create-plan.html', context)
     else:
