6514b89debd8abb0d492684ef760cca78d1e7941
KrystianGrabowski <krystian987543@gmail.com>
2019-12-09 15:51:45 +0100

    Dodano filtr konwertujący liczbę na dzień tygodnia

---
+++ zapisy/apps/schedule/templatetags/schedule_filters.py
@@ -0,0 +1,19 @@
+"""Template Tags useful in schedule views."""
+from django import template
+
+register = template.Library()
+
+
+@register.filter(is_safe=True)
+def number_to_weekday(value: str):
+    """Translates a number into a weekday."""
+    types_dict = {
+        '1': "pn",
+        '2': "wt",
+        '3': "sr",
+        '4': "czw",
+        '5': "pt",
+        '6': "so",
+        '0': "ni",
+    }
+    return types_dict.get(value, "")
