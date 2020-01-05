90bfffba4ccc5629f8571ca6a5d30257beb3035a
Karol Guzikowski <karolguzikowski@gmail.com>
2020-01-05 19:37:58 +0100

    changed plan-create template(added 2 links)

--- zapisy/apps/offer/plan/views.py
+++ zapisy/apps/offer/plan/views.py
@@ -8,6 +8,8 @@ from apps.offer.plan.utils import get_votes, propose, get_subjects_data
 
 VOTING_RESULTS_SPREADSHEET_ID = '1pfLThuoKf4wxirnMXLi0OEksIBubWpjyrSJ7vTqrb-M'
 PLAN_PROPOSAL_SPREADSHEET_ID = '17fDGtuZVUZlUztXqtBn1tuOqkZjCTPJUb14p5YQrnck'
+CLASS_ASSIGNMENT_SPREADSHEET_ID = '1jy195Cvfly7SJ1BI_-eBDjB4tx706ra35GCdFqmGDVM'
+EMPLOYEES_SPREADSHEET_ID = '1OGvQLfekTF5qRAZyYSkSi164WtnDwsI1RUEDz80nyhY'
 
 
 def plan_view(request):
@@ -33,7 +35,9 @@ def plan_create(request):
         context = {
             'courses_proposal': courses,
             'voting_results_sheet_id': VOTING_RESULTS_SPREADSHEET_ID,
-            'plan_proposal_sheet_id': PLAN_PROPOSAL_SPREADSHEET_ID
+            'plan_proposal_sheet_id': PLAN_PROPOSAL_SPREADSHEET_ID,
+            'class_assignment_sheet_id': CLASS_ASSIGNMENT_SPREADSHEET_ID,
+            'employees_sheet_id': EMPLOYEES_SPREADSHEET_ID
         }
         return render(request, 'plan/create-plan.html', context)
     else:
