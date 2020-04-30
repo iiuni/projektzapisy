7df3b807e5342a66e5fb12606720f539fcbc8de5
Jan Marcinkowski <jasiekmarc@gmail.com>
2020-04-30 15:31:00 +0200

    MarkdownEditor sam importuje swoje skrypty.

--- zapisy/apps/offer/proposal/assets/markdown-editor.js
+++ zapisy/apps/offer/proposal/assets/markdown-editor.js
@@ -1,9 +1,15 @@
-import Vue from "vue/dist/vue.js";
+import Vue from "vue/dist/vue";
 import MarkdownEditor from "./MarkdownEditor.vue";
 
-new Vue({
-    el: "#edit-proposal-form",
-    components: {
-        "markdown-editor": MarkdownEditor
-    }
-});
+// We import the version of Vue that contains template compiler. It only affects
+// very few users, and allows us to write <markdown-editor ...> in the template.
+// This suppresses the console log.
+Vue.config.productionTip = false;
+Vue.config.devtools = false;
+
+// <markdown-editor ...> will be a child of the previous sibling of the script
+// element.
+const me = document.currentScript;
+const el = me.previousElementSibling;
+
+new Vue({ el: el, components: { "markdown-editor": MarkdownEditor } });
