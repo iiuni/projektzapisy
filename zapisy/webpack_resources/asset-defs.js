25c7f5d4b22a0128de6aaf3836877a52ba9bc243
Jasiek Busz <jasiekbusz@gmail.com>
2020-06-19 17:33:39 +0200

    Fixed asset-defs file

--- zapisy/webpack_resources/asset-defs.js
+++ zapisy/webpack_resources/asset-defs.js
@@ -44,80 +44,101 @@ const AssetDefs = {
 
   // Poll app
 
-  "poll-bokeh-plotting": ["bokeh.js", "bokeh.scss"],
+  "poll-bokeh-plotting": [
+    path.resolve("apps/grade/poll/assets/bokeh.js"),
+    path.resolve("apps/grade/poll/assets/bokeh.scss"),
+  ],
 
   // Ticket_create app
 
   "ticket_create-katex": [
-    "apps/grade/ticket_create/assets/katex.ts",
+    path.resolve(
+      "apps/grade/ticket_create/assets/katex.ts"
+    ),
   ],
   "ticket_create-ticketsgenerate": [
-    "apps/grade/ticket_create/assets/ticketsgenerate_main.js",
+    path.resolve(
+      "apps/grade/ticket_create/assets/ticketsgenerate_main.js"
+    ),
   ],
 
   // Notification app
 
   "notifications-notifications-widget": [
-    "apps/notifications/assets/notifications-widget.js",
+    path.resolve(
+      "apps/notifications/assets/notifications-widget.js"
+    ),
   ],
 
   // Desiderata app
 
   "desiderata-checkboxes": [
-    "apps/offer/desiderata/assets/checkboxes-toggling.js",
+    path.resolve(
+      "apps/offer/desiderata/assets/checkboxes-toggling.js"
+    ),
   ],
 
   // Proposal app
 
   "proposal-fill-placeholders": [
-    "apps/offer/proposal/assets/fill-placeholders.ts",
+    path.resolve(
+      "apps/offer/proposal/assets/fill-placeholders.ts"
+    ),
   ],
   "proposal-course-list": [
-    "apps/offer/proposal/assets/course-list-index.js",
+    path.resolve(
+      "apps/offer/proposal/assets/course-list-index.js"
+    ),
   ],
 
   // Vote app
 
   "vote-point-counter": [
-    "apps/offer/vote/assets/point-counter.ts",
+    path.resolve("apps/offer/vote/assets/point-counter.ts"),
   ],
   "vote-bootstrap-table": [
-    "apps/offer/vote/assets/bootstrap-table.js",
+    path.resolve(
+      "apps/offer/vote/assets/bootstrap-table.js"
+    ),
   ],
 
   // Schedule app
 
   "schedule-reservation-widget": [
-    "apps/schedule/assets/reservation-widget.js",
+    path.resolve(
+      "apps/schedule/assets/reservation-widget.js"
+    ),
   ],
   "schedule-reservation": [
-    "apps/schedule/assets/reservation.js",
+    path.resolve("apps/schedule/assets/reservation.js"),
   ],
   "schedule-fullcalendar": [
-    "apps/schedule/assets/fullcalendar.ts",
+    path.resolve("apps/schedule/assets/fullcalendar.ts"),
   ],
   "schedule-report": [
-    "apps/schedule/assets/report.js",
-    "apps/schedule/assets/report.css",
+    path.resolve("apps/schedule/assets/report.js"),
+    path.resolve("apps/schedule/assets/report.css"),
   ],
   "schedule-report-editor": [
-    "apps/schedule/assets/report-editor.ts",
-    "apps/schedule/assets/report-editor.scss",
+    path.resolve("apps/schedule/assets/report-editor.ts"),
+    path.resolve("apps/schedule/assets/report-editor.scss"),
   ],
 
   // Theses app
 
   "theses-theses-widget": [
-    "apps/theses/assets/theses-widget.js",
+    path.resolve("apps/theses/assets/theses-widget.js"),
   ],
 
   // User app
 
   "user-user-filter": [
-    "apps/user/assets/user-filter-list-index.js",
+    path.resolve(
+      "apps/users/assets/user-filter-list-index.js"
+    ),
   ],
   "user-consent-dialog": [
-    "apps/user/assets/consent-dialog.ts",
+    path.resolve("apps/users/assets/consent-dialog.ts"),
   ],
 };
 
