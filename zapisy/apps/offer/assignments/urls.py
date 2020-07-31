31b0ff6b67f01bd620a873621d58a9a56bc051fc
Jan Marcinkowski <jasiekmarc@gmail.com>
2020-07-31 20:20:28 +0200

    Drobne.

--- zapisy/apps/offer/assignments/urls.py
+++ zapisy/apps/offer/assignments/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.plan_view, name='assignments-view'),
-    path('create/', views.assignments_wizard, name='assignments-wizard'),
-    path('create/assignments', views.create_assignments_sheet, name='create-assignments-sheet'),
+    path('wizard/', views.assignments_wizard, name='assignments-wizard'),
+    path('wizard/assignments', views.create_assignments_sheet, name='create-assignments-sheet'),
     # create voting results sheet
-    path('create/voting', views.create_voting_sheet, name='create-voting-sheet'),
+    path('wizard/voting', views.create_voting_sheet, name='create-voting-sheet'),
     # generate scheduler file
-    path('create/scheduler/<slug:semester>/<slug:fmt>',
+    path('wizard/scheduler/<slug:semester>/<slug:fmt>',
          views.generate_scheduler_file,
          name='generate-scheduler-file'),
 ]
