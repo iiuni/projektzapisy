02dec7e1d0bc18467d7b59a2cc72a160e4516db7
Jan Marcinkowski <jasiekmarc@gmail.com>
2020-09-10 02:14:09 +0200

    Cookieconsent z paczki.

--- zapisy/apps/common/assets/cookieconsent/display-cookieconsent.ts
+++ zapisy/apps/common/assets/cookieconsent/display-cookieconsent.ts
@@ -1,10 +1,7 @@
 // Displays a EU-required Cookie information bar at the bottom of the screen
 // unless the user had already dismissed it.
-//
-// Uses cookieconsent v2 library, downloaded into our project in 2016 (see #18).
-
-import "./cookieconsent.min.js";
-import "./cookieconsent.min.css";
+import "cookieconsent";
+import "cookieconsent/build/cookieconsent.min.css";
 
 window.addEventListener("load", function () {
   (window as any).cookieconsent.initialise({
