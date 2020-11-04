7d79f2e0a69ff81f434d8090706457ec639006bb
Jan Marcinkowski <jasiekmarc@gmail.com>
2020-11-04 17:27:40 +0100

    Linting.

--- zapisy/apps/theses/assets/store/theses.ts
+++ zapisy/apps/theses/assets/store/theses.ts
@@ -5,7 +5,7 @@ export interface ThesisInfo {
   id: number;
   title: string;
   is_available: boolean;
-  reserved_until: string,
+  reserved_until: string;
   kind: string;
   status: string;
   modified: number;
