9a3861084a0e7581f4d2382044474e5a81c3248a
Piotr Łężny <plezny@gmail.com>
2020-12-18 19:48:05 +0100

    Modele do aplikacji Effects

--- zapisy/apps/effects/models.py
+++ zapisy/apps/effects/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from apps.users.models import Student
+from apps.enrollment.courses.models import CourseInstance, Effects
+
+
+# Model for completed courses
+class CompletedCourses(models.Model):
+    # id?
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    courses = models.ManyToManyField(CourseInstance, blank=True, null=True)
+    # program = ?   Czy to jest potrzebne? Program studiów jest również w modelu studenta.
+
+
+# Model for completed effects
+class CompletedEffects(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    effects = models.ManyToManyField(Effects, blank=True, null=True)
+    # program = ?
