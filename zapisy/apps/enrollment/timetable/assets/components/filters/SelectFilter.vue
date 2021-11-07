78308aa48439c2c92de77993918399c2025a572a
Jakub Mendyk <jakubmendyk.szkola+git@gmail.com>
2021-11-07 13:00:44 +0100

    Fix linter issues

--- zapisy/apps/enrollment/timetable/assets/components/filters/SelectFilter.vue
+++ zapisy/apps/enrollment/timetable/assets/components/filters/SelectFilter.vue
@@ -33,11 +33,11 @@ export default Vue.extend({
       selected: undefined,
     };
   },
-  created: function() {
+  created: function () {
     const searchParams = new URL(window.location.href).searchParams;
     const isChosenKey = ([key, _]) => searchParams.get(this.filterKey) == key;
 
-    if(searchParams.has(this.filterKey) && this.options.some(isChosenKey)) {
+    if (searchParams.has(this.filterKey) && this.options.some(isChosenKey)) {
       // Set selection from URL only if respective key is in search params
       // and its value is a valid option.
       this.$data.selected = searchParams.get(this.filterKey);
