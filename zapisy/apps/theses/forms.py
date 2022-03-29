2841b9fa0d4cbf904dc6753e87f1e33cac87e4b0
adiwili <adiwilinska@gmail.com>
2022-03-29 12:37:20 +0200

    add default date to the sent empty reserved_until field

--- zapisy/apps/theses/forms.py
+++ zapisy/apps/theses/forms.py
@@ -187,6 +187,11 @@ class EditThesisForm(ThesisFormBase):
                 instance.status = ThesisStatus.ACCEPTED.value
             else:
                 instance.status = status
+        
+        if "students" not in self.data:
+            instance.reserved_until = None
+        elif instance.reserved_until is None:
+            instance.reserved_until = datetime.date.today()+datetime.timedelta(days=730)
 
         if commit:
             instance.save()
