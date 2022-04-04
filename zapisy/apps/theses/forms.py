fa2b4941fda18574d52a46b0a89834aadc86bef8
MariaSzlasa <maria.szlasa@gmail.com>
2022-04-04 18:03:05 +0200

    small fix

--- zapisy/apps/theses/forms.py
+++ zapisy/apps/theses/forms.py
@@ -4,7 +4,6 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Column, Layout, Row, Submit
 from django import forms
 from django.utils import timezone
-import datetime
 
 from apps.common import widgets as common_widgets
 from apps.theses.enums import ThesisKind, ThesisStatus, ThesisVote
