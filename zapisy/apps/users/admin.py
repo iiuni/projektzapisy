e3aaaa7a07e103e18fccdd928b1fea4b84c02aa4
michalzielinskiuwr <86728841+michalzielinskiuwr@users.noreply.github.com>
2022-04-11 19:15:34 +0200

    Update admin.py
    linter issues 4

--- zapisy/apps/users/admin.py
+++ zapisy/apps/users/admin.py
@@ -52,11 +52,10 @@ class StudentAdmin(admin.ModelAdmin):
         """The opening times refreshing concerns the upcoming semester"""
 
         semester = Semester.get_semester(Semester.get_current_semester().semester_ending + datetime.timedelta(days=1))
-	
         if semester is None:
             self.message_user(
-		    	request, "Aby uaktualnic czasy nalezy najpierw utworzyc przyszly semestr",
-			level=messages.WARNING)
+                    request, "Aby uaktualnic czasy nalezy najpierw utworzyc przyszly semestr",
+                    level=messages.WARNING)
             return
         if semester.records_opening is None:
             self.message_user(request, "Prosze uzupelnic szczegoly przyszlego semestru", level=messages.WARNING)
@@ -67,9 +66,9 @@ class StudentAdmin(admin.ModelAdmin):
             T0Times.populate_t0_selected(semester, student)
             GroupOpeningTimes.populate_opening_times_selected(semester, student)
         self.message_user(
-		request,
-        	"Obliczono czasy otwarcia zapisów.",
-        	level=messages.SUCCESS)
+                request,
+                "Obliczono czasy otwarcia zapisów.",
+                level=messages.SUCCESS)
 
     refresh_opening_times.short_description = "Oblicz czasy otwarcia zapisów"
 
