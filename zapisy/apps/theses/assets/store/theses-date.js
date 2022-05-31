1d398f2fbc7b23408bffc0bd51698b0659e0b97a
adiwili <adiwilinska@gmail.com>
2022-05-31 16:20:18 +0200

    calculating the date using the dayjs library

--- zapisy/apps/theses/assets/store/theses-date.js
+++ zapisy/apps/theses/assets/store/theses-date.js
@@ -1,53 +1,53 @@
-window.onload = function () {
-  function changeOfDateField(requiredDate, reservedUntil) {
-    if (requiredDate) {
-      var reservedUntilDate = new Date();
-      reservedUntilDate.setFullYear(reservedUntilDate.getFullYear() + 2);
-      var dd = String(reservedUntilDate.getDate()).padStart(2, "0");
-      var mm = String(reservedUntilDate.getMonth() + 1).padStart(2, "0");
-      var yyyy = reservedUntilDate.getFullYear();
-      reservedUntilDate = yyyy + "-" + mm + "-" + dd;
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import duration from "dayjs/plugin/duration";
 
-      reservedUntil.value = reservedUntilDate;
-      reservedUntil.disabled = false;
-      reservedUntil.required = true;
-    } else {
-      reservedUntil.value = "";
-      reservedUntil.disabled = true;
-      reservedUntil.required = false;
-    }
+dayjs.extend(customParseFormat);
+dayjs.extend(duration);
 
-    reservedUntil.classList.add("border-danger");
-    var intervalID = setInterval(function () {
-      if (reservedUntil.classList.contains("border-danger")) {
-        reservedUntil.classList.remove("border-danger");
-      }
-    }, 1500);
-  }
-
-  var studentsSelect = document.getElementById("id_students");
-  var reservedUntil = document.getElementById("id_reserved_until");
-
-  var requiredDate = Boolean(
-    studentsSelect.querySelectorAll("option:checked").length
-  );
-
-  if (!Boolean(reservedUntil.value) && !reservedUntil.disabled) {
+function changeOfDateField(requiredDate, reservedUntil) {
+  if (requiredDate) {
+    reservedUntil.value = dayjs()
+      .add(dayjs.duration({ years: 2 }))
+      .format("YYYY-MM-DD");
+    reservedUntil.disabled = false;
+    reservedUntil.required = true;
+  } else {
+    reservedUntil.value = "";
     reservedUntil.disabled = true;
+    reservedUntil.required = false;
   }
-  if (Boolean(reservedUntil.value) != requiredDate) {
+
+  reservedUntil.classList.add("border-danger");
+  var intervalID = setInterval(function () {
+    if (reservedUntil.classList.contains("border-danger")) {
+      reservedUntil.classList.remove("border-danger");
+    }
+  }, 1500);
+}
+
+var studentsSelect = document.getElementById("id_students");
+var reservedUntil = document.getElementById("id_reserved_until");
+
+var requiredDate = Boolean(
+  studentsSelect.querySelectorAll("option:checked").length
+);
+
+if (!Boolean(reservedUntil.value) && !reservedUntil.disabled) {
+  reservedUntil.disabled = true;
+}
+if (Boolean(reservedUntil.value) != requiredDate) {
+  changeOfDateField(requiredDate, reservedUntil);
+}
+
+studentsSelect.addEventListener("change", function handleChange(event) {
+  if (
+    Boolean(studentsSelect.querySelectorAll("option:checked").length) !=
+    requiredDate
+  ) {
+    requiredDate = !requiredDate;
+    changeOfDateField(requiredDate, reservedUntil);
+  } else if (Boolean(reservedUntil.value) != requiredDate) {
     changeOfDateField(requiredDate, reservedUntil);
   }
-
-  studentsSelect.addEventListener("change", function handleChange(event) {
-    if (
-      Boolean(studentsSelect.querySelectorAll("option:checked").length) !=
-      requiredDate
-    ) {
-      requiredDate = !requiredDate;
-      changeOfDateField(requiredDate, reservedUntil);
-    } else if (Boolean(reservedUntil.value) != requiredDate) {
-      changeOfDateField(requiredDate, reservedUntil);
-    }
-  });
-};
+});
