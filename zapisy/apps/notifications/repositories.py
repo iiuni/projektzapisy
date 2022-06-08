b9884db7cfef89ddbe16beacd7900c1423bc1ec0
Dominik Danelski <dominik@danelski.pl>
2022-06-08 16:54:40 +0200

    Pleasing flake8

--- zapisy/apps/notifications/repositories.py
+++ zapisy/apps/notifications/repositories.py
@@ -3,11 +3,12 @@ from datetime import datetime
 from typing import List
 
 import redis
-from apps.notifications.datatypes import Notification
-from apps.notifications.serialization import JsonNotificationSerializer, NotificationSerializer
+
 from django.contrib.auth.models import User
 
 from apps.common.redis import flush_with_prefix
+from apps.notifications.datatypes import Notification
+from apps.notifications.serialization import JsonNotificationSerializer, NotificationSerializer
 
 KEY_PREFIX = 'notifications:'
 KEY_PATTERN = KEY_PREFIX + '*'
