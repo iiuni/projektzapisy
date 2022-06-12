a0c445e6e0cfc7cfd27eacd343d41a598c1a5e71
latarniakonto <wisniewskikq27@gmail.com>
2022-06-12 17:44:36 +0200

    Add checkboxes controlling display

--- zapisy/apps/enrollment/timetable/assets/components/PrototypeSummary.vue
+++ zapisy/apps/enrollment/timetable/assets/components/PrototypeSummary.vue
@@ -30,6 +30,10 @@ export default class PrototypeSummary extends Vue {
   //   // The same goes for courses and tester.
   //   courses!: CourseInfo[];
   //   tester!: (_: CourseInfo) => boolean;
+  public summary1: Boolean = false;
+  public summary2: Boolean = false;
+  public summary3: Boolean = false;
+  public summary4: Boolean = false;
 
   get totalPoints(): number {
     return state.state.totalPoints;
@@ -86,25 +90,47 @@ export default class PrototypeSummary extends Vue {
 
 <template>
   <div class="table-responsiveVUE">
+    <ul>
+      <li class="summary">
+        <input v-model="summary1" type="checkbox" value="" id="summary1" />
+        <label for="summary1"> (P, Z, K, L) </label><br />
+      </li>
+      <li class="summary">
+        <input v-model="summary2" type="checkbox" value="" id="summary2" />
+        <label for="summary2"> (Z) </label><br />
+      </li>
+      <li class="summary">
+        <input v-model="summary3" type="checkbox" value="" id="summary3" />
+        <label for="summary3"> (K) </label><br />
+      </li>
+      <li class="summary">
+        <input v-model="summary4" type="checkbox" value="" id="summary4" />
+        <label for="summary4"> (P) </label><br />
+      </li>
+    </ul>
     <SimpleSummary
+      v-if="summary1"
       summaryType="(P, Z, K, L)"
       :sumPoints="totalPoints"
       :groups="groups"
       :courses="totalCourses"
     />
     <SimpleSummary
+      v-if="summary2"
       summaryType="(Z)"
       :sumPoints="enrolledPoints"
       :groups="groups"
       :courses="enrolledCourses"
     />
     <SimpleSummary
+      v-if="summary3"
       summaryType="(K)"
       :sumPoints="queuedPoints"
       :groups="groups"
       :courses="queuedCourses"
     />
     <SimpleSummary
+      v-if="summary4"
       summaryType="(P)"
       :sumPoints="pinnedPoints"
       :groups="groups"
@@ -112,3 +138,9 @@ export default class PrototypeSummary extends Vue {
     />
   </div>
 </template>
+
+<style>
+.summary {
+  display: inline-block;
+}
+</style>
