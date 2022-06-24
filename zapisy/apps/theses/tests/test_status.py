3132f8de7722f727ee7c1ca32bee67a8c762cf03
MariaSzlasa <maria.szlasa@gmail.com>
2022-06-24 11:04:22 +0200

    Revert "dodanie wciecia do testu - teraz jest w klasie"
    This reverts commit c3bae4b323b06ca44d6b78fc99057d3133eea9f0.

--- zapisy/apps/theses/tests/test_status.py
+++ zapisy/apps/theses/tests/test_status.py
@@ -154,22 +154,22 @@ class ThesisStatusChangeTestCase(TestCase):
         self.assertRaises(ValueError, thesis_form_1.save, commit=True)
 
 
-    def test_reservation_date_valid(self):
-        form_data_0 = {'title': 'Praca dyplomowa',
-                    'advisor': self.thesis_owner, 'kind': 0,
-                    'reserved_until': timezone.now(),
-                    'students': [StudentFactory(), StudentFactory()],
-                    'max_number_of_students': 2}
+def test_reservation_date_valid(self):
+    form_data_0 = {'title': 'Praca dyplomowa',
+                   'advisor': self.thesis_owner, 'kind': 0,
+                   'reserved_until': timezone.now(),
+                   'students': [StudentFactory(), StudentFactory()],
+                   'max_number_of_students': 2}
 
-        form_data_1 = {'title': 'Praca dyplomowa',
-                    'advisor': self.thesis_owner, 'kind': 0,
-                    'max_number_of_students': 2}
+    form_data_1 = {'title': 'Praca dyplomowa',
+                   'advisor': self.thesis_owner, 'kind': 0,
+                   'max_number_of_students': 2}
 
-        thesis_form_0 = ThesisForm(user=self.thesis_owner.user, data=form_data_0)
-        thesis_form_1 = ThesisForm(user=self.thesis_owner.user, data=form_data_1)
+    thesis_form_0 = ThesisForm(user=self.thesis_owner.user, data=form_data_0)
+    thesis_form_1 = ThesisForm(user=self.thesis_owner.user, data=form_data_1)
 
-        thesis_form_0.save(commit=True)
-        thesis_form_1.save(commit=True)
+    thesis_form_0.save(commit=True)
+    thesis_form_1.save(commit=True)
 
-        self.assertTrue(thesis_form_0.is_valid())
-        self.assertTrue(thesis_form_1.is_valid())
+    self.assertTrue(thesis_form_0.is_valid())
+    self.assertTrue(thesis_form_1.is_valid())
