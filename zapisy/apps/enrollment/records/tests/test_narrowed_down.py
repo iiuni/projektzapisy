69317d33f203c876bd2a2f7a6128569d7999544b
michalzielinskiuwr <86728841+michalzielinskiuwr@users.noreply.github.com>
2022-06-27 18:04:39 +0200

    Update test_narrowed_down.py

--- zapisy/apps/enrollment/records/tests/test_narrowed_down.py
+++ zapisy/apps/enrollment/records/tests/test_narrowed_down.py
@@ -27,13 +27,13 @@ class OpeningTimesTest(TestCase):
         GroupOpeningTimes.populate_opening_times(cls.semester, groups=[self.knitting_lecture_group])
         
         tola_knitting_opening = GroupOpeningTimes.objects.get(
-            student=self.tola, group=self.knitting_lecture_group).time
+            student=cls.tola, group=cls.knitting_lecture_group).time
         lolek_knitting_opening = GroupOpeningTimes.objects.get(
-            student=self.lolek, group=self.knitting_lecture_group).time
+            student=cls.lolek, group=cls.knitting_lecture_group).time
         cls.diff = lolek_knitting_opening - tola_knitting_opening
         
         #Tola gets an earlier opening time
-        GroupOpeningTimes.objects.filter(student=cls.tola, group=self.knitting_lecture_group).update(time=tola_knitting_opening + 2 * cls.diff)
+        GroupOpeningTimes.objects.filter(student=cls.tola, group=cls.knitting_lecture_group).update(time=tola_knitting_opening + 2 * cls.diff)
         
         #Populate opening times for knitting lecture, but only for Bolek and Lolek now.
         GroupOpeningTimes.populate_opening_times(
@@ -49,4 +49,4 @@ class OpeningTimesTest(TestCase):
             student=self.tola, group=self.knitting_lecture_group).time
         lolek_knitting_opening = GroupOpeningTimes.objects.get(
             student=self.lolek, group=self.knitting_lecture_group).time
-        assert bolek_knitting_opening - lolek_knitting_opening == 2 * cls.diff
+        assert bolek_knitting_opening - lolek_knitting_opening == 2 * self.diff
