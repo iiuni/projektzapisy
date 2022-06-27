b5e0571899bc98cb45a5cbce2893bbf974fecb34
michalzielinskiuwr <86728841+michalzielinskiuwr@users.noreply.github.com>
2022-06-27 18:56:35 +0200

    Update test_opening_times.py

--- zapisy/apps/enrollment/records/tests/test_opening_times.py
+++ zapisy/apps/enrollment/records/tests/test_opening_times.py
@@ -121,3 +121,17 @@ class OpeningTimesTest(TestCase):
                 self.bolek, [self.washing_up_seminar_group],
                 self.washing_up_seminar_group.course.records_start +
                 timedelta(seconds=1))[self.washing_up_seminar_group.id])
+        
+        def test_narrowed_down(self):
+            """This tests GroupOpeningTimes for special cases."""
+            bolek_knitting_opening = GroupOpeningTimes.objects.get(
+            student=self.bolek, group=self.knitting_lecture_group).time
+            lolek_knitting_opening = GroupOpeningTimes.objects.get(
+                student=self.lolek, group=self.knitting_lecture_group).time
+            GroupOpeningTimes.objects.filter(
+                student=self.bolek,
+                group=self.knitting_lecture_group
+            ).update(time=bolek_knitting_opening + timedelta(hours=1))
+            GroupOpeningTimes.populate_opening_times(cls.semester,students=[self.lolek])
+            assert bolek_knitting_opening - lolek_knitting_opening == timedelta(hours=2)
+            
