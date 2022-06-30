0ea13b945f78529627efe92245b17dad1b7fe20d
latarniakonto <wisniewskikq27@gmail.com>
2022-06-30 11:44:22 +0200

    Move summary to single table

--- zapisy/apps/enrollment/timetable/assets/components/PrototypeSummary.vue
+++ zapisy/apps/enrollment/timetable/assets/components/PrototypeSummary.vue
@@ -1,51 +1,19 @@
 <script lang="ts">
 import Vue from "vue";
-// import { mapGetters } from "vuex";
 import Component from "vue-class-component";
 import { values, cloneDeep } from "lodash";
-// import { getCurrentInstance } from "vue";
 
-// const current = getCurrentInstance();
 import state from "../store/groups";
-import { DayOfWeek, nameDay, Course, Group } from "../models";
+import { Course, Group } from "../models";
 import SimpleSummary from "./SimpleSummary.vue";
 
-// export type CourseObject = { id: number; name: string; url: string };
 @Component({
   components: {
     SimpleSummary,
   },
-  computed: {
-    // ...mapGetters("courses", {
-    //   sumPointsState: "sumPoints",
-    // }),
-    // ...mapGetters("filters", {
-    //   tester: "visible",
-    // }),
-  },
 })
 export default class PrototypeSummary extends Vue {
-  // The computed property selectionState comes from store.
-  //   selectionState!: number[];
-  //   // The same goes for courses and tester.
-  //   courses!: CourseInfo[];
-  //   tester!: (_: CourseInfo) => boolean;
-  public summary1: Boolean = false;
-  public summary2: Boolean = false;
-  public summary3: Boolean = false;
-  public summary4: Boolean = false;
-
-  public selectedCourses: { [id: number]: Course } = {};
-  public selectedPoints: number = 0;
-
-  public pinnedCourses: { [id: number]: Course } = {};
-  public pinnedPoints: number = 0;
-
-  public enqueuedCourses: { [id: number]: Course } = {};
-  public enqueuedPoints: number = 0;
-
-  public enrolledCourses: { [id: number]: Course } = {};
-  public enrolledPoints: number = 0;
+  public points: number = 0;
 
   public selectedGroups: Function = (group: Group, course: Course) =>
     !group.isEnrolled &&
@@ -65,7 +33,7 @@ export default class PrototypeSummary extends Vue {
 
   public enrolledGroups: Function = (group: Group, course: Course) =>
     group.isEnrolled && group.course.id == course.id;
-  
+
   public initializeCourses() {
     let groups: { [id: number]: Group } = values(state.state.store).filter(
       (g) => g.isEnrolled || g.isEnqueued || g.isPinned || g.isSelected
@@ -73,58 +41,85 @@ export default class PrototypeSummary extends Vue {
     let enrolledCourses = {};
     let enrolledPoints = 0;
     Object.entries(groups).forEach((g) => {
-          if (g[1].isEnrolled && enrolledCourses[g[1].course.id] === undefined) {
-            enrolledCourses[g[1].course.id] = cloneDeep(g[1].course);
-            enrolledCourses[g[1].course.id].summaryPoints = enrolledCourses[g[1].course.id].points;
-            enrolledPoints += enrolledCourses[g[1].course.id].summaryPoints;
-          }
-        });
+      if (g[1].isEnrolled && enrolledCourses[g[1].course.id] === undefined) {
+        enrolledCourses[g[1].course.id] = cloneDeep(g[1].course);
+        enrolledCourses[g[1].course.id].summaryPoints =
+          enrolledCourses[g[1].course.id].points;
+        enrolledPoints += enrolledCourses[g[1].course.id].summaryPoints;
+      }
+    });
     let enqueuedCourses = {};
     let enqueuedPoints = 0;
     Object.entries(groups).forEach((g) => {
       if (g[1].isEnqueued && enqueuedCourses[g[1].course.id] === undefined) {
         enqueuedCourses[g[1].course.id] = cloneDeep(g[1].course);
         if (enrolledCourses[g[1].course.id] === undefined) {
-              enqueuedCourses[g[1].course.id].summaryPoints = enqueuedCourses[g[1].course.id].points;
-            } else {
-              enqueuedCourses[g[1].course.id].summaryPoints = 0;
-            }
+          enqueuedCourses[g[1].course.id].summaryPoints =
+            enqueuedCourses[g[1].course.id].points;
+        } else {
+          enqueuedCourses[g[1].course.id].summaryPoints = 0;
+        }
         enrolledPoints += enqueuedCourses[g[1].course.id].summaryPoints;
       }
     });
     let pinnedCourses = {};
     let pinnedPoints = 0;
     Object.entries(groups).forEach((g) => {
-      if (g[1].isPinned && !g[1].isEnrolled && !g[1].isEnqueued && pinnedCourses[g[1].course.id] === undefined) {
+      if (
+        g[1].isPinned &&
+        !g[1].isEnrolled &&
+        !g[1].isEnqueued &&
+        pinnedCourses[g[1].course.id] === undefined
+      ) {
         pinnedCourses[g[1].course.id] = cloneDeep(g[1].course);
-        if (enrolledCourses[g[1].course.id] === undefined && 
-            enqueuedCourses[g[1].course.id] === undefined) {              
-              pinnedCourses[g[1].course.id].summaryPoints = pinnedCourses[g[1].course.id].points;
-            } else {
-              pinnedCourses[g[1].course.id].summaryPoints = 0;
-            }
+        if (
+          enrolledCourses[g[1].course.id] === undefined &&
+          enqueuedCourses[g[1].course.id] === undefined
+        ) {
+          pinnedCourses[g[1].course.id].summaryPoints =
+            pinnedCourses[g[1].course.id].points;
+        } else {
+          pinnedCourses[g[1].course.id].summaryPoints = 0;
+        }
         pinnedPoints += pinnedCourses[g[1].course.id].summaryPoints;
       }
     });
     let selectedCourses = {};
     let selectedPoints = 0;
     Object.entries(groups).forEach((g) => {
-      if (g[1].isSelected && !g[1].isPinned && !g[1].isEnrolled && !g[1].isEnqueued && selectedCourses[g[1].course.id] === undefined) {
+      if (
+        g[1].isSelected &&
+        !g[1].isPinned &&
+        !g[1].isEnrolled &&
+        !g[1].isEnqueued &&
+        selectedCourses[g[1].course.id] === undefined
+      ) {
         selectedCourses[g[1].course.id] = cloneDeep(g[1].course);
-        if (enrolledCourses[g[1].course.id] === undefined && 
-            enqueuedCourses[g[1].course.id] === undefined && 
-            pinnedCourses[g[1].course.id] === undefined) {              
-              selectedCourses[g[1].course.id].summaryPoints = selectedCourses[g[1].course.id].points;
-            } else {
-              selectedCourses[g[1].course.id].summaryPoints = 0;
-            }
+        if (
+          enrolledCourses[g[1].course.id] === undefined &&
+          enqueuedCourses[g[1].course.id] === undefined &&
+          pinnedCourses[g[1].course.id] === undefined
+        ) {
+          selectedCourses[g[1].course.id].summaryPoints =
+            selectedCourses[g[1].course.id].points;
+        } else {
+          selectedCourses[g[1].course.id].summaryPoints = 0;
+        }
         selectedPoints += selectedCourses[g[1].course.id].summaryPoints;
       }
     });
-    return {enrolledCourses: enrolledCourses, enqueuedCourses: enqueuedCourses, 
-            pinnedCourses: pinnedCourses, selectedCourses: selectedCourses, 
-            enrolledPoints: enrolledPoints, enqueuedPoints: enqueuedPoints,
-            pinnedPoints: pinnedPoints, selectedPoints: selectedPoints};
+    this.points =
+      selectedPoints + pinnedPoints + enqueuedPoints + enrolledPoints;
+    return {
+      enrolledCourses: enrolledCourses,
+      enqueuedCourses: enqueuedCourses,
+      pinnedCourses: pinnedCourses,
+      selectedCourses: selectedCourses,
+      enrolledPoints: enrolledPoints,
+      enqueuedPoints: enqueuedPoints,
+      pinnedPoints: pinnedPoints,
+      selectedPoints: selectedPoints,
+    };
   }
 
   get pinnedSummary(): [{ [id: number]: Course }, number] {
@@ -152,54 +147,18 @@ export default class PrototypeSummary extends Vue {
       (g) => g.isEnrolled || g.isEnqueued || g.isPinned || g.isSelected
     );
   }
-  public GetNameDay(day: DayOfWeek) {
-    return nameDay(day);
-  }
-  // set sumPoints(newValue: number): {
-  //   state.state.sumPoints = newValue;
-  // }
-
-  // The list should be initialised to contain courses filtered with initial filters
-  // fetched from the query string and then apply filters whenever they update.
-  //   visibleCourses: CourseInfo[] = [];
-  //   methods: {
-  // this.visibleCourses = this.courses.filter(this.tester);
-
-  // this.$store.subscribe((mutation, state) => {
-  //   switch (mutation.type) {
-  //     case "filters/registerFilter":
-  //       this.visibleCourses = this.courses.filter(this.tester);
-  //       break;
-  //   }
-  // });
-  //   }
 }
 </script>
 
 <template>
   <div class="table-responsiveVUE">
-    <button type="button" class="summary" @click="summary1 = !summary1">
-      (L)
-    </button>
-    <button type="button" class="summary" @click="summary2 = !summary2">
-      (Z)
-    </button>
-    <button type="button" class="summary" @click="summary3 = !summary3">
-      (K)
-    </button>
-    <button type="button" class="summary" @click="summary4 = !summary4">
-      (P)
-    </button>
-    <div v-if="summary1">
-      <SimpleSummary
-        summaryType="(L)"
-        :groups="groups"
-        :groupsCondition="selectedGroups"
-        :courses="selectedSummary[0]"
-        :points="selectedSummary[1]"
-      />
-    </div>
-    <div v-if="summary2">
+    <table id="enr-schedule-listByCourseVUE" class="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">Przedmiot</th>
+          <th class="ects" scope="col">ECTS</th>
+        </tr>
+      </thead>
       <SimpleSummary
         summaryType="(Z)"
         :groups="groups"
@@ -207,8 +166,6 @@ export default class PrototypeSummary extends Vue {
         :courses="enrolledSummary[0]"
         :points="enrolledSummary[1]"
       />
-    </div>
-    <div v-if="summary3">
       <SimpleSummary
         summaryType="(K)"
         :groups="groups"
@@ -216,21 +173,37 @@ export default class PrototypeSummary extends Vue {
         :courses="enqueuedSummary[0]"
         :points="enqueuedSummary[1]"
       />
-    </div>
-    <div v-if="summary4">
       <SimpleSummary
-        v-if="summary4"
+        summaryType="(P)"
         :groups="groups"
         :groupsCondition="pinnedGroups"
         :courses="pinnedSummary[0]"
         :points="pinnedSummary[1]"
       />
-    </div>    
+      <SimpleSummary
+        summaryType="(L)"
+        :groups="groups"
+        :groupsCondition="selectedGroups"
+        :courses="selectedSummary[0]"
+        :points="selectedSummary[1]"
+      />
+      <tfoot>
+        <tr>
+          <td>
+            <strong>Suma punktów ECTS łącznie:</strong>
+          </td>
+          <td class="ects">{{ points }}</td>
+        </tr>
+      </tfoot>
+    </table>
   </div>
 </template>
 
 <style>
-.summary {
-  display: inline-block;
+thead {
+  width: 100%;
+}
+th.ects {
+  text-align: right;
 }
 </style>
--- zapisy/apps/enrollment/timetable/assets/components/SimpleSummary.vue
+++ zapisy/apps/enrollment/timetable/assets/components/SimpleSummary.vue
@@ -1,134 +1,95 @@
 <script lang="ts">
 import Vue from "vue";
-// import { mapGetters } from "vuex";
 import Component from "vue-class-component";
-// import { getCurrentInstance } from "vue";
-// const current = getCurrentInstance();
-import { DayOfWeek, nameDay, Group } from "../models";  
+import { DayOfWeek, nameDay, Group } from "../models";
 
-// export type CourseObject = { id: number; name: string; url: string };
 const SimpleSummaryProps = Vue.extend({
   props: {
     summaryType: {
       type: String,
       default: "",
-    },    
+    },
     groups: {
       type: Array as () => Array<Group>,
       default: {},
-    },    
+    },
     groupsCondition: {
       type: Function,
     },
     courses: {
       type: Object,
-      default: {}
+      default: {},
     },
     points: {
       type: Number,
       default: 0,
-    }
+    },
   },
 });
-@Component({
-  computed: {
-    // ...mapGetters("courses", {
-      //   sumPointsState: "sumPoints",
-    // }),
-    // ...mapGetters("filters", {
-      //   tester: "visible",
-    // }),
-  },
-})
+@Component({})
 export default class SimpleSummary extends SimpleSummaryProps {
-  // The computed property selectionState comes from store.
-  //   selectionState!: number[];
-  //   // The same goes for courses and tester.
-  //   courses!: CourseInfo[];
-  //   tester!: (_: CourseInfo) => boolean;
-  //   get sumPoints(): number {
-    //     return state.state.sumPoints;
-  //   }
-  //   get courses(): { [id: number]: Course } {
-    //     return state.state.courses;
-  //   }
-  //   get groups(): { [id: number]: Group } {
-  //     return values(state.state.store).filter(
-    //       (g) => g.isEnrolled || g.isEnqueued || g.isPinned || g.isSelected
-  //     );
-  //   }
   public GetNameDay(day: DayOfWeek) {
     return nameDay(day);
   }
-  // set sumPoints(newValue: number): {
-    //   state.state.sumPoints = newValue;
-  // }
-
-  // The list should be initialised to contain courses filtered with initial filters
-  // fetched from the query string and then apply filters whenever they update.
-  //   visibleCourses: CourseInfo[] = [];
-  //   methods: {
-    // this.visibleCourses = this.courses.filter(this.tester);
-
-  // this.$store.subscribe((mutation, state) => {
-    //   switch (mutation.type) {
-      //     case "filters/registerFilter":
-  //       this.visibleCourses = this.courses.filter(this.tester);
-  //       break;
-  //   }
-  // });
-  //   }
 }
 </script>
 
 <template>
-  <div class="table-responsiveVUE">
-    <table id="enr-schedule-listByCourseVUE" class="table table-striped">
-      <thead>
-        <tr>
-          <th scope="col">Przedmiot</th>
-          <th class="ects" scope="col">ECTS</th>
-        </tr>
-      </thead>
-      <tfoot>
-        <tr>
-          <td>
-            <strong>Suma punktów ECTS za {{ summaryType }}:</strong>
-          </td>
-          <td class="ects">{{ points }}</td>
-        </tr>
-      </tfoot>
-      <tbody v-for="(item, idx) in courses" :value="item" :key="idx">
-        <tr class="courseHeader">
-          <td class="name" scope="col">
-            <a href="{% url 'course-page' course.grouper.slug %}">
-              {{ item.name }}
-            </a>
-          </td>
-          <td rowspan="2" class="ects">
-            {{ item.summaryPoints }}
-          </td>
-        </tr>
-        <tr class="courseDetails">
-          <td>
-            <ul v-for="(group, gid) in groups" :value="group" :key="gid">
-              <li v-if="groupsCondition(group, item)">
-                <span class="type">{{ group.type }}:</span>
-                <span class="term">
-                  {{ GetNameDay(group.terms[0].weekday) }}
-                  {{ group.terms[0].startTimeString }}-{{
-                    group.terms[0].endTimeString
-                  }}
-                </span>
-                <span class="classroom"
-                  >sala:
-                  {{ group.terms[0].getClassrooms }}
-                </span>
-              </li>
-            </ul>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+  <div>
+    <tbody>
+      <tr>
+        <td>
+          <strong>Suma punktów ECTS za {{ summaryType }}:</strong>
+        </td>
+        <td class="ects">{{ points }}</td>
+      </tr>
+    </tbody>
+    <tbody v-for="(item, idx) in courses" :value="item" :key="idx">
+      <tr class="courseHeader">
+        <td class="name" scope="col">
+          <a href="{% url 'course-page' course.grouper.slug %}">
+            {{ item.name }}
+          </a>
+        </td>
+        <td rowspan="2" class="ects" align="right">
+          {{ item.summaryPoints }}
+        </td>
+      </tr>
+      <tr class="courseDetails">
+        <td class="courseDetails">
+          <ul v-for="(group, gid) in groups" :value="group" :key="gid">
+            <li v-if="groupsCondition(group, item)">
+              <span class="type">{{ group.type }}:</span>
+              <span class="term">
+                {{ GetNameDay(group.terms[0].weekday) }}
+                {{ group.terms[0].startTimeString }}-{{
+                  group.terms[0].endTimeString
+                }}
+              </span>
+              <span class="classroom"
+                >sala:
+                {{ group.terms[0].getClassrooms }}
+              </span>
+            </li>
+          </ul>
+        </td>
+      </tr>
+    </tbody>
   </div>
 </template>
+
+<style>
+tbody {
+  width: 114%;
+  display: table;
+}
+td.courseDetails {
+  width: 90%;
+}
+tr.simpleSumamry {
+  width: 200%;
+}
+td.ects {
+  text-align: right;
+}
+</style>
