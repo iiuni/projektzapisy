2ca44d26cb54eb3814abd1a2f2fc98a7b4904391
Maria0123 <57042818+Maria0123@users.noreply.github.com>
2022-07-09 09:02:42 +0200

    Wycofanie zmian w regeksie

--- zapisy/apps/schedule/urls.py
+++ zapisy/apps/schedule/urls.py
@@ -4,8 +4,7 @@ from . import feeds, views
 
 urlpatterns = [
     path('classrooms/', views.classrooms, name='classrooms'),
-    re_path(r'^classrooms/get_terms/(?P<year>[0-9]{4})-(?P<month>[1-9]|0[1-9]|1[0-2])-\
-            (?P<day>[1-9]|0[1-9]|[12][0-9]|3[01])/$',
+    re_path(r'^classrooms/get_terms/(?P<year>[0-9]{4})-(?P<month>[0-9]{1,2})-(?P<day>[0-9]{1,2})/$',
             views.get_terms,
             name='get_terms'),
     path('classrooms/reservation/', views.new_reservation, name='reservation'),
