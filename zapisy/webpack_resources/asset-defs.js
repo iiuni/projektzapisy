a1de3d40b05651cffa0af42e931000f6485d0e8e
wilinska <adrianna.wilinska@protonmail.com>
2022-07-12 13:20:24 +0200

    Przeniesienie skryptu do osobnego pliku

--- zapisy/webpack_resources/asset-defs.js
+++ zapisy/webpack_resources/asset-defs.js
@@ -99,7 +99,7 @@ const AssetDefs = {
   // Theses app
 
   "theses-theses-widget": [path.resolve("apps/theses/assets/theses-widget.js")],
-
+  "theses-theses-change": [path.resolve("apps/theses/assets/theses-change.js")],
   // User app
 
   "users-user-filter": [
