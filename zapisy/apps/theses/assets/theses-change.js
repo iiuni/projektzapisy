c11030afbe48b05d15a7dc43c97de7c805a68989
wilinska <adrianna.wilinska@protonmail.com>
2022-07-12 15:21:20 +0200

    usuniecie jquery z eventu

--- zapisy/apps/theses/assets/theses-change.js
+++ zapisy/apps/theses/assets/theses-change.js
@@ -3,33 +3,39 @@ window.onload = function () {
     document.getElementById("old_instance").textContent
   );
 
-  $(document).on("submit", ".confirm-submit", function () {
-    const confirm_msg =
-      "Zapisanie zmian spowoduje ponowne przesłanie pracy do komisji.\n" +
-      "Czy na pewno chcesz zapisać zmiany w pracy dyplomowej?";
-    if (document.querySelector('[name="title"]').value != old_instance.title) {
-      return confirm(`Zmieniono tytuł pracy.\n${confirm_msg}`);
-    }
-    if (
-      document.querySelector('[name="supporting_advisor"]').value !=
-      old_instance.supporting_advisor
-    ) {
-      return confirm(`Zmieniono promotora wspierającego.\n${confirm_msg}`);
-    }
-    if (document.querySelector('[name="kind"]').value != old_instance.kind) {
-      return confirm(`Zmieniono typ pracy.\n${confirm_msg}`);
-    }
-    if (
-      document.querySelector('[name="max_number_of_students"]').value !=
-      old_instance.max_number_of_students
-    ) {
-      return confirm(`Zmieniono maks. liczba studentów.\n${confirm_msg}`);
-    }
-    if (
-      document.querySelector('textarea[name="description"]').value !=
-      old_instance.description
-    ) {
-      return confirm(`Zmieniono opis pracy.\n${confirm_msg}`);
-    }
-  });
+  document.querySelector(".confirm-submit").addEventListener(
+    "submit",
+    function () {
+      const confirm_msg =
+        "Zapisanie zmian spowoduje ponowne przesłanie pracy do komisji.\n" +
+        "Czy na pewno chcesz zapisać zmiany w pracy dyplomowej?";
+      if (
+        document.querySelector('[name="title"]').value != old_instance.title
+      ) {
+        return confirm(`Zmieniono tytuł pracy.\n${confirm_msg}`);
+      }
+      if (
+        document.querySelector('[name="supporting_advisor"]').value !=
+        old_instance.supporting_advisor
+      ) {
+        return confirm(`Zmieniono promotora wspierającego.\n${confirm_msg}`);
+      }
+      if (document.querySelector('[name="kind"]').value != old_instance.kind) {
+        return confirm(`Zmieniono typ pracy.\n${confirm_msg}`);
+      }
+      if (
+        document.querySelector('[name="max_number_of_students"]').value !=
+        old_instance.max_number_of_students
+      ) {
+        return confirm(`Zmieniono maks. liczba studentów.\n${confirm_msg}`);
+      }
+      if (
+        document.querySelector('textarea[name="description"]').value !=
+        old_instance.description
+      ) {
+        return confirm(`Zmieniono opis pracy.\n${confirm_msg}`);
+      }
+    },
+    false
+  );
 };
