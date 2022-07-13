caa596899d8e4d9b1a8cb7e4ad5feb3878f4bb54
michalzielinskiuwr <86728841+michalzielinskiuwr@users.noreply.github.com>
2022-07-14 01:19:56 +0200

    Update views.py

--- zapisy/apps/grade/poll/views.py
+++ zapisy/apps/grade/poll/views.py
@@ -195,8 +195,9 @@ class PollResults(TemplateView):
             )
 
         last_modifieds: Dict[Poll, datetime.datetime] = dict(
-            Submission.objects.filter(poll__in=polls, submitted=True).order_by('poll',
-                        'modified').distinct('poll').values_list("poll", "modified")
+            Submission.objects.filter(poll__in=polls,
+                submitted=True).order_by('poll',
+                    'modified').distinct('poll').values_list("poll", "modified")
         )
         for poll in polls:
             if poll.id in last_views:
