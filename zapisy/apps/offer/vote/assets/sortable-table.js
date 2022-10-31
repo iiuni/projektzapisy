4ef59940914b417d4534dfcc51e6e7d2aa9709ae
Tomek <tomasz.wolczanski@proton.me>
2022-10-31 21:56:49 +0100

    made a small change and added comments

--- zapisy/apps/offer/vote/assets/sortable-table.js
+++ zapisy/apps/offer/vote/assets/sortable-table.js
@@ -1,5 +1,7 @@
 import * as $ from "jquery";
 
+// col - column to be sorted
+// order - ascending (0) or descending (1)
 function sort(proposals, col, order) {
   const sorted = $(proposals)
     .find("tr")
@@ -10,6 +12,7 @@ function sort(proposals, col, order) {
       if (order === 1) {
         [a, b] = [b, a];
       }
+      // the first column contains text
       if (col === 0) {
         return new Intl.Collator("pl").compare(a, b);
       } else {
@@ -26,38 +29,53 @@ function sort(proposals, col, order) {
   $(proposals).empty().append(sorted);
 }
 
+// fontawesome classes
+// orderClasses[0] - class for ascending order
+// orderClasses[1] - class for descending order
+// orderClasses[2] - class for no order
 const orderClasses = ["fa-sort-up", "fa-sort-down", "fa-sort"];
 
 $(function () {
   $(".table .headers").each((i, headers) => {
+    // get proposals for the current headers
     const proposals = $(".table .proposals").eq(i);
+    // ignore the last column
     const ths = $(headers).find("th").slice(0, 3);
+    // initially proposals are sorted by the first column in ascending order
     let col = 0;
     let order = 0;
+
     $(ths).each((j, th) => {
       const text = $(th).text();
       if (j === col) {
+        // insert the icon and text into a flexbox container
         $(th)
           .empty()
           .append(
-            `<div class="th-content">${text}<i class="fa fa-sort-up"></i></div>`
+            `<div class="th-content">${text}<i class="fa ${orderClasses[0]}"></i></div>`
           );
+        // initial sort
         sort(proposals, col, order);
       } else {
         $(th)
           .empty()
           .append(
-            `<div class="th-content">${text}<i class="fa fa-sort"></i></div>`
+            `<div class="th-content">${text}<i class="fa ${orderClasses[2]}"></i></div>`
           );
       }
+
       $(th).on("click", () => {
+        // the th is in the column that the proposals are sorted by
         if (j === col) {
+          // toggle between ascending and descending order
           order = (order + 1) % 2;
+          // update the icon
           $(th)
             .find("[data-fa-i2svg]")
             .removeClass(orderClasses)
             .addClass(orderClasses[order]);
         } else {
+          // update the icons
           $(ths)
             .eq(col)
             .find("[data-fa-i2svg]")
@@ -67,6 +85,7 @@ $(function () {
             .find("[data-fa-i2svg]")
             .removeClass(orderClasses)
             .addClass(orderClasses[0]);
+          // update the column and set the order to ascending
           col = j;
           order = 0;
         }
