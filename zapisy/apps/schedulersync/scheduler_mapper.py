290dcd8f6e78be7b7063914c3873fc01501bc992
receek <krzysztof.juszczyk20@gmail.com>
2023-02-02 12:42:42 +0100

    Scheduler mapper checks that exists CourseInstance object for semester and course name

--- zapisy/apps/schedulersync/scheduler_mapper.py
+++ zapisy/apps/schedulersync/scheduler_mapper.py
@@ -152,16 +152,25 @@ class SchedulerMapper:
                                     "the course proposal in the database.\n"
                                     "Type 'None' to mark the course to be ignored.".format(error, name))
 
-        def get_course(proposal: 'Proposal') -> 'CourseInstance':
+        def get_course(proposal: 'Proposal', course_name: 'str', interactive: bool = True) -> 'CourseInstance':
             """Return CourseInstance object from SZ database."""
             course = None
             try:
                 course = CourseInstance.objects.get(semester=self.semester, offer=proposal)
                 self.summary.used_courses += 1
             except CourseInstance.DoesNotExist:
-                course = CourseInstance.create_proposal_instance(proposal, self.semester)
-                self.summary.created_courses += 1
-                print(f"+ Course instance '{proposal.name}' created")
+                existing_course = CourseInstance.objects.filter(semester=self.semester, name=course_name)
+                if not existing_course:
+                    course = CourseInstance.create_proposal_instance(proposal, self.semester)
+                    self.summary.created_courses += 1
+                    print(f"+ Course instance '{proposal.name}' created")
+                elif interactive:
+                    existing_proposal = existing_course.get().proposal
+                    print(
+                        f"Course {course_name} has created CourseInstance object for proposal: {existing_proposal}. "
+                        f"Skiping create new CourseInsatnce object for proposal: {proposal}. "
+                        "Please fix it."
+                    )
             return course
 
         mapped_courses = {}
@@ -170,7 +179,7 @@ class SchedulerMapper:
             if proposal is None:
                 mapped_courses[course] = None
             else:
-                mapped_courses[course] = get_course(proposal)
+                mapped_courses[course] = get_course(proposal, course, self.interactive_flag)
         return mapped_courses
 
     def _map_classrooms(self, rooms: Iterable[str]) -> Dict[str, Classroom]:
