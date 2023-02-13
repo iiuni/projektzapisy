beb70377f8c763698c90e99bb4f5588b266f26f6
adamuro <horosporo@gmail.com>
2023-02-13 23:31:30 +0100

    Dodanie filtrowanie po języku wykładu, refaktor

--- zapisy/apps/effects/admin.py
+++ zapisy/apps/effects/admin.py
@@ -1,25 +1,22 @@
 import json
-import logging
 
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 
 from apps.effects.models import Variant
 
-logger = logging.getLogger()
-
 
 @admin.register(Variant)
 class VariantAdmin(admin.ModelAdmin):
     list_display = ('name', 'program', 'requirements_formatted', 'pdf_link')
 
-    def requirements_formatted(self, obj):
+    def requirements_formatted(self, obj: Variant):
         formatted = json.dumps(
-            obj.requirements.get('wymagania'), indent=4, ensure_ascii=False
-        ).encode('utf-8').decode().replace('\n', '<br>').replace('    ', '&emsp;')
+            obj.requirements.get('wymagania'), indent=2, ensure_ascii=False,
+        ).encode('utf-8').decode().replace('\n', '<br>').replace('  ', '&emsp;')
         return mark_safe(formatted)
 
-    def pdf_link(self, obj):
+    def pdf_link(self, obj: Variant):
         url = obj.requirements.get("meta").get("url")
         link = f'<a target="_blank" rel="noopener noreferrer" href={url}>Link</a>'
         return mark_safe(link)
--- zapisy/apps/effects/views.py
+++ zapisy/apps/effects/views.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Union, Iterable
 
 from django.shortcuts import render
 
@@ -12,41 +13,78 @@ from .models import Student
 logger = logging.getLogger()
 
 
+def update_course_types(requirement: dict, summary: dict) -> Iterable[CourseType]:
+    if 'typy_przedmiotow' not in requirement:
+        return CourseType.objects.all()
+
+    summary.update({'types': requirement.get('typy_przedmiotow')})
+    return CourseType.objects.filter(short_name__in=requirement.get('typy_przedmiotow'))
+
+
+def update_course_tags(requirement: dict, summary: dict) -> Iterable[CourseTag]:
+    if 'tagi_przedmiotow' not in requirement:
+        return CourseTag.objects.all()
+
+    summary.update({'tags': requirement.get('tagi_przedmiotow')})
+    return CourseTag.objects.filter(short_name__in=requirement.get('tagi_przedmiotow'))
+
+
+def update_course_languages(requirement: dict, summary: dict) -> "list[str]":
+    if 'jezyk_wykladowy' not in requirement:
+        return ['pl', 'en']
+
+    summary.update({'language': requirement.get('jezyk_wykladowy')})
+    return [requirement.get('jezyk_wykladowy')]
+
+
+def update_additional_info(requirement: dict, summary: dict) -> Union[str, None]:
+    summary.update({'additional_info': requirement.get('dodatkowe_info')})
+    return requirement.get('dodatkowe_info')
+
+
+def update_courses(requirement: dict, courses: int, summary: dict) -> int:
+    courses_needed = max(requirement.get('liczba_przedmiotow') - courses, 0)
+    summary.update({'courses': courses_needed})
+    if courses_needed > 0:
+        summary.update({'fulfilled': False})
+    if requirement.get('liczba_przedmiotow') == 1:
+        summary.update({'course_done': courses_needed == 0})
+    return courses_needed
+
+
+def update_ects(requirement: dict, ects: int, summary: dict) -> int:
+    ects_needed = max(requirement.get('liczba_ects') - ects, 0)
+    summary.update({'ects': ects_needed})
+    if ects_needed > 0:
+        summary.update({'fulfilled': False})
+    return ects_needed
+
+
+def update_alternatives(requirement: dict, student: Student, summary: dict) -> "list[dict]":
+    alternatives = [get_requirement_summary(student, req) for req in requirement.get('alternatywnie')]
+    summary.update({'alternatively': alternatives})
+    if all([not alternative.get('fulfilled') for alternative in alternatives]):
+        summary.update({'fulfilled': False})
+    return alternatives
+
+
 def get_requirement_summary(student: Student, requirement: dict) -> dict:
-    summary = {'fulfilled': True}
+    summary = {'fulfilled': True, 'name': requirement.get('nazwa')}
+
+    course_types = update_course_types(requirement, summary)
+    course_tags = update_course_tags(requirement, summary)
+    course_languages = update_course_languages(requirement, summary)
+    count, ects = CompletedCourses.get_count_and_ects(student, course_types, course_tags, course_languages)
+
     if 'dodatkowe_info' in requirement:
-        summary.update({'additional_info': requirement.get('dodatkowe_info')})
-
-    course_types, course_tags = None, None
-    if 'typy_przedmiotow' in requirement:
-        summary.update({'types': requirement.get('typy_przedmiotow')})
-        course_types = CourseType.objects.filter(short_name__in=requirement.get('typy_przedmiotow'))
-    if 'tagi_przedmiotow' in requirement:
-        summary.update({'tags': requirement.get('tagi_przedmiotow')})
-        course_tags = CourseTag.objects.filter(short_name__in=requirement.get('tagi_przedmiotow'))
-
-    courses, ects = CompletedCourses.get_count_and_ects_by_types_and_tags(student, course_types, course_tags)
+        update_additional_info(requirement, summary)
     if 'liczba_przedmiotow' in requirement:
-        courses_needed = max(requirement.get('liczba_przedmiotow') - courses, 0)
-        summary.update({'courses': courses_needed})
-        if courses_needed > 0:
-            summary.update({'fulfilled': False})
-        if requirement.get('liczba_przedmiotow') == 1:
-            summary.update({'course_done': courses_needed == 0})
+        update_courses(requirement, count, summary)
     if 'liczba_ects' in requirement:
-        ects_needed = max(requirement.get('liczba_ects') - ects, 0)
-        summary.update({'ects': ects_needed})
-        if ects_needed > 0:
-            summary.update({'fulfilled': False})
-    if 'jezyk_wykladowy' in requirement:
-        # TODO
-        pass
+        update_ects(requirement, ects, summary)
     if 'alternatywnie' in requirement:
-        alternatives = [get_requirement_summary(student, requirement)
-                        for requirement in requirement.get('alternatywnie')]
-        summary.update(min(alternatives, key=lambda alt: alt['ects'] if 'ects' in alt else alt['courses']))
+        update_alternatives(requirement, student, summary)
 
-    summary.update({'name': requirement.get('nazwa')})
     return summary
 
 
@@ -56,13 +94,12 @@ def my_studies(request):
 
     The page displays user progress in graduating the university.
     """
-    data = {'requirements': []}
+    data = {}
     student: Student = request.user.student
 
     variant = Variant.objects.filter(program=student.program).first()
     requirements: list[dict] = variant.requirements.get('wymagania')
-    for requirement in requirements:
-        requirement_summary = get_requirement_summary(student, requirement)
-        data.get('requirements').append(requirement_summary)
+    summaries = [get_requirement_summary(student, requirement) for requirement in requirements]
+    data.update({'requirements': summaries})
 
     return render(request, 'my_studies.html', data)
