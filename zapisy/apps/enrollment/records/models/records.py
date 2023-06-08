cc8eb6c6dfcdecba90e0fadfb67f468cd784d2cf
Bartosz Cieślik <bartoszcieslik2@gmail.com>
2023-06-08 15:33:45 +0200

    poprawa dokumentacji

--- zapisy/apps/enrollment/records/models/records.py
+++ zapisy/apps/enrollment/records/models/records.py
@@ -1,42 +1,4 @@
-"""Module records is the heart of enrollment logic.
-
-Record Lifetime:
-
-  The record's status transitions in one direction: QUEUED -> ENROLLED ->
-  REMOVED. The ENROLLED phase may be skipped if the student removes his record
-  while he is in the queue, or if the enrollment is unsuccessful. The REMOVED
-  phase may not occur if the student ends up being enrolled into the group.
-
-  enqueue_student(student, group): The life of a record begins with this
-    function, which puts the student into the queue of the group. The record
-    will not be created if the function `can_enqueue` does not pass.
-
-    Immediately after records are created, an asynchronous task is placed (in
-    the task queue), to pull as many records in these groups as possible from
-    their respective queues. The tasks are placed using a signal
-    (GROUP_CHANGE_SIGNAL) defined in `apps/enrollment/records/signals.py`. They
-    are picked up by the function `pull_from_queue_signal_receiver` in
-    `apps/enrollment/records/tasks.py`.
-
-  fill_group(group_id): The asynchronous task runs this function. It is a loop
-    calling `pull_record_into_group` as long as it returns True, which means
-    that there still place in the group and students in the queue.
-
-  pull_record_into_group(group_id): Picks the first student in the group's queue
-    and tries to enroll him in the group using `enroll_or_remove`.
-
-  enroll_or_remove(record): Takes the record and tries to change its status from
-    QUEUED to ENROLLED. This operation will be unsuccessful if the function
-    `can_enroll` does not pass, in which case the record's status will be
-    changed to REMOVED. This function additionally removes the student from all
-    the parallel groups upon enrolling him into this one, and removes him from
-    all the queues of lower priority.
-
-  remove_from_group(student, group): Removes student from the group or its
-    queue, thus changing the record's status to REMOVED. All the groups that are
-    vacated by the student must be filled by an asynchronous process, so the
-    GROUP_CHANGE_SIGNAL is sent.
-"""
+"""All enrollment login was moved to engine module."""
 
 import logging
 from enum import Enum
