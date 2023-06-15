c52882e43901eca290098ca51a3d7d4e4fc10085
Bogusz Kaszowski <261841@uwr.edu.pl>
2023-06-15 23:14:02 +0200

    fix linter issue

--- zapisy/apps/users/assets/consent-dialog.ts
+++ zapisy/apps/users/assets/consent-dialog.ts
@@ -1,8 +1,8 @@
 // Displays the data consent modal right when the page loads.
 import * as bootstrap from "bootstrap";
 
-window.onload = function() {
-  const modal = document.getElementById('consentDialog');
+window.onload = function () {
+  const modal = document.getElementById("consentDialog");
 
   if (modal) {
     const bootstrapModal = new bootstrap.Modal(modal, {});
