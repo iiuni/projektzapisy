04dc842f547b3fd5ec8c2f3066374134280cde00
Bogusz Kaszowski <261841@uwr.edu.pl>
2023-06-17 23:54:01 +0200

    fix loading consent-dialog script

--- zapisy/apps/users/assets/consent-dialog.ts
+++ zapisy/apps/users/assets/consent-dialog.ts
@@ -1,11 +1,11 @@
 // Displays the data consent modal right when the page loads.
 import * as bootstrap from "bootstrap";
 
-window.onload = function () {
+window.addEventListener("load", () => {
   const modal = document.getElementById("consentDialog");
 
   if (modal) {
     const bootstrapModal = new bootstrap.Modal(modal, {});
     bootstrapModal.show();
   }
-};
+});
