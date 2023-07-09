c06a1647646910d9403197dcdddf1cc0d5c80e4d
Patryk Zieliński <mrzbiznes@gmail.com>
2023-07-10 01:44:46 +0200

    Include attachments to slack msg

--- infra/db_backups/slack_notifications.py
+++ infra/db_backups/slack_notifications.py
@@ -12,22 +12,23 @@ def get_connected_slack_client(secrets_env):
     #raise RuntimeError('SlackClient.rtm_connect failed')
 
 
-def _send_slack_msg(slack_client, channel_id: str, msg: str):
+def _send_slack_msg(slack_client, channel_id: str, msg: str, attachments):
     slack_client.chat_postMessage(
         channel=channel_id,
-        text=msg
+        text=msg,
+        attachments=attachments
     )
 
 
-def send_success_notification(slack_client, dev_db_link: str, seconds_elapsed: int, channel_id: str, attempts: int):
+def send_success_notification(slack_client, dev_db_link: str, seconds_elapsed: int, channel_id: str, attempts: int, attachments):
     if attempts == 1:
         msg = f'Databases backed up successfully in {seconds_elapsed} seconds. *Dev DB download link:* {dev_db_link}'
     else:
         msg = f'Databases backed up successfully in {seconds_elapsed} seconds, after {attempts} attempts. *Dev DB download link:* {dev_db_link} \nFailed attempt(s) tracebacks:'
         
-    _send_slack_msg(slack_client, channel_id, msg)
+    _send_slack_msg(slack_client, channel_id, msg, attachments)
 
 
-def send_error_notification(slack_client, error_msg: str, channel_id: str):
-    msg = f'*Failed to back up databases:*\n```{error_msg}```'
-    _send_slack_msg(slack_client, channel_id, msg)
+def send_error_notification(slack_client, error_msg: str, channel_id: str, attachments):
+    msg = f'*Failed to back up databases. Failed attempt(s) tracebacks:'
+    _send_slack_msg(slack_client, channel_id, msg, attachments)
