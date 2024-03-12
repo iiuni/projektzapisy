6c44da464b69e13f511f2480e77405f36aaebb23
Wiktor Ogrodnik <wiktor2800-o@outlook.com>
2024-03-12 18:29:37 +0100

    Usunięto nieużywane importy

--- zapisy/apps/enrollment/records/models/opening_times.py
+++ zapisy/apps/enrollment/records/models/opening_times.py
@@ -9,7 +9,6 @@ from collections import defaultdict
 from datetime import datetime, timedelta
 from typing import Dict, Iterable, List
 
-from django.conf import settings
 from django.db import models, transaction
 
 from apps.enrollment.courses.models import CourseInstance, Group, Semester
