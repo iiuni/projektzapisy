96154248897ce1a9725f9bf3732140b4e9001081
Martyna Szymańska <jamczylija@gmail.com>
2024-05-20 01:59:05 +0200

    Walka z prettierem

--- zapisy/apps/common/assets/main/expose_libs.ts
+++ zapisy/apps/common/assets/main/expose_libs.ts
@@ -10,12 +10,12 @@ window.addEventListener("load", () => {
   Array.from(document.querySelectorAll('[data-bs-toggle="popover"]')).map(
     function (popoverTriggerEl) {
       return new bootstrap.Popover(popoverTriggerEl);
-    },
+    }
   );
+
   Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]')).map(
     function (tooltipTriggerEl) {
       return new bootstrap.Tooltip(tooltipTriggerEl);
-    },
+    }
   );
 });
-
