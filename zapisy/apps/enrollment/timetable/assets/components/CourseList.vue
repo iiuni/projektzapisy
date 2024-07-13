c2eb5b1a4ac2eb2850103260ebdeb4273f050715
mbaugustyn <michal.augustyn@outlook.com>
2024-07-13 16:58:03 +0200

    refactor courselist from timetable

--- zapisy/apps/enrollment/timetable/assets/components/CourseList.vue
+++ zapisy/apps/enrollment/timetable/assets/components/CourseList.vue
@@ -1,58 +1,46 @@
-<script lang="ts">
+<script setup lang="ts">
 // The CourseList component allows the student to select courses presented on
 // prototype.
 //
 // The selection is not persistent. In order to keep a group on prototype the
 // student will need to _pin_ it. The state is not maintained by the component.
 // This job is handled by the Vuex store (`../store/courses.ts`).
-import Vue from "vue";
-import { mapGetters } from "vuex";
-import Component from "vue-class-component";
-
+import { ComputedRef } from "vue";
+import { onMounted, ref } from "vue";
 import { CourseInfo } from "../store/courses";
-
 export type CourseObject = { id: number; name: string; url: string };
 
-@Component({
-  computed: {
-    ...mapGetters("courses", {
-      selectionState: "selection",
-      courses: "courses",
-    }),
-    ...mapGetters("filters", {
-      tester: "visible",
-    }),
-  },
-})
-export default class CourseList extends Vue {
-  // The computed property selectionState comes from store.
-  selectionState!: number[];
-  // The same goes for courses and tester.
-  courses!: CourseInfo[];
-  tester!: (_: CourseInfo) => boolean;
+import { computed, getCurrentInstance } from "vue";
+// TODO: use store from vuex4
+const useStore = () => {
+  const vm = getCurrentInstance();
+  if (!vm) throw new Error("must be called in setup");
+  return vm.proxy!.$store;
+};
+const store = useStore();
 
-  get selection(): number[] {
-    return this.selectionState;
-  }
-  set selection(value: number[]) {
-    this.$store.dispatch("courses/updateSelection", value);
-  }
+const courses: ComputedRef<CourseInfo[]> = computed(
+  () => store.getters["courses/courses"]
+);
+const tester = computed(() => store.getters["filters/visible"]);
+const selection = computed({
+  get: () => store.state.courses.selection,
+  set: (value: number[]) => store.dispatch("courses/updateSelection", value),
+});
 
-  // The list should be initialised to contain courses filtered with initial filters
-  // fetched from the query string and then apply filters whenever they update.
-  visibleCourses: CourseInfo[] = [];
-  mounted() {
-    this.visibleCourses = this.courses.filter(this.tester);
+const visibleCourses = ref<CourseInfo[]>([]);
 
-    this.$store.subscribe((mutation, state) => {
-      switch (mutation.type) {
-        case "filters/registerFilter":
-          this.visibleCourses = this.courses.filter(this.tester);
-          break;
-      }
-    });
-  }
-}
+onMounted(() => {
+  visibleCourses.value = courses.value.filter(tester.value);
+  // TODO do we subscribe in onMounted or setup?
+  store.subscribe((mutation, state) => {
+    switch (mutation.type) {
+      case "filters/registerFilter":
+        visibleCourses.value = courses.value.filter(tester.value);
+        break;
+    }
+  });
+});
 </script>
 
 <template>
@@ -69,12 +57,14 @@ export default class CourseList extends Vue {
         >
           <input
             type="checkbox"
-            :id="c.id"
+            :id="'' + c.id"
             :value="c.id"
             v-model="selection"
             class="custom-control-input"
           />
-          <label :for="c.id" class="custom-control-label">{{ c.name }}</label>
+          <label :for="'' + c.id" class="custom-control-label">{{
+            c.name
+          }}</label>
         </li>
       </ul>
     </div>
