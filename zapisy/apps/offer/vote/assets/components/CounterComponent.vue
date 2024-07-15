ae2354f0b263510993d6ec28fa44f8f63004a022
mbaugustyn <michal.augustyn@outlook.com>
2024-07-15 21:28:18 +0200

    Refactor CounterComponent

--- zapisy/apps/offer/vote/assets/components/CounterComponent.vue
+++ zapisy/apps/offer/vote/assets/components/CounterComponent.vue
@@ -1,32 +1,20 @@
+<script setup lang="ts">
+import { sum, values } from "lodash";
+import { computed } from "vue";
+// The component displaying the message will get two inputs. One is a point
+// limit, the other is a map of point values from input fields, keyed by the
+// input id.
+const props = defineProps<{
+  inputs: { [key: string]: number };
+  limit: number;
+}>();
+
+const total = computed(() => sum(values(props.inputs)));
+const exceedsLimit = computed(() => total.value > props.limit);
+</script>
+
 <template>
   <div class="alert alert-info" :class="{ 'alert-danger': exceedsLimit }">
     Wykorzystano {{ total }} z {{ limit }} punktów
   </div>
 </template>
-
-<script lang="ts">
-import Vue from "vue";
-import Component from "vue-class-component";
-import { sum, values } from "lodash";
-
-// The component displaying the message will get two inputs. One is a point
-// limit, the other is a map of point values from input fields, keyed by the
-// input id.
-const counterComponentProps = Vue.extend({
-  props: {
-    inputs: Object as () => { [key: string]: number },
-    limit: Number,
-  },
-});
-@Component
-export default class CounterComponent extends counterComponentProps {
-  // Computes the total number of points in inputs.
-  get total(): number {
-    return sum(values(this.inputs));
-  }
-
-  get exceedsLimit(): boolean {
-    return this.total > this.limit;
-  }
-}
-</script>
