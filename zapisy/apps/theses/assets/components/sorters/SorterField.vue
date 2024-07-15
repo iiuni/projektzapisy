b0fd03630ab33a90acd7d03819f970f8f9a8ba53
mbaugustyn <michal.augustyn@outlook.com>
2024-07-15 21:22:51 +0200

    Refactor SorterField

--- zapisy/apps/theses/assets/components/sorters/SorterField.vue
+++ zapisy/apps/theses/assets/components/sorters/SorterField.vue
@@ -1,52 +1,54 @@
-<script lang="ts">
-import Vue from "vue";
-import { mapMutations, mapGetters } from "vuex";
+<script setup lang="ts">
+import { computed, onMounted } from "vue";
+import { ref } from "vue";
 
-// TextFilter applies the string filtering on a property of a thesis.
-export default Vue.extend({
-  props: {
-    // Property of a thesis on which we are filtering.
-    property: String,
-    label: String,
-  },
-  data: () => {
-    return {
-      order: 0,
-    };
-  },
-  mounted: function () {
-    this.$store.subscribe((mutation, state) => {
-      switch (mutation.type) {
-        case "sorting/changeSorting":
-          if (this.getSortingProperty != this.property) this.order = 0;
-          break;
-      }
-    });
-  },
-  computed: {
-    ...mapGetters("sorting", {
-      getSortingProperty: "getProperty",
-    }),
-  },
-  methods: {
-    ...mapMutations("sorting", ["changeSorting"]),
-    sort: function () {
-      if (this.order == 2) {
-        this.changeSorting({
-          k: "modified",
-          f: false,
-        });
-        this.order = 0;
-      } else {
-        this.changeSorting({
-          k: this.property,
-          f: this.order === 0,
-        });
-        this.order += 1;
-      }
-    },
-  },
+// TODO unify defineProps
+const props = defineProps({
+  property: String,
+  label: String,
 });
+
+import { getCurrentInstance } from "vue";
+// TODO: use store from vuex4
+const useStore = () => {
+  const vm = getCurrentInstance();
+  if (!vm) throw new Error("must be called in setup");
+  return vm.proxy!.$store;
+};
+const store = useStore();
+
+const order = ref(0);
+onMounted(() => {
+  store.subscribe((mutation) => {
+    switch (mutation.type) {
+      case "sorting/changeSorting":
+        if (getSortingProperty.value != props.property) order.value = 0;
+        break;
+    }
+  });
+});
+const getSortingProperty = computed(() => {
+  return store.getters["sorting/getProperty"];
+});
+
+const changeSorting = ({ k, f }) => {
+  store.commit("sorting/changeSorting", { k, f });
+};
+const sort = () => {
+  if (order.value == 2) {
+    changeSorting({
+      k: "modified",
+      f: false,
+    });
+    order.value = 0;
+  } else {
+    changeSorting({
+      k: props.property,
+      f: order.value === 0,
+    });
+    order.value += 1;
+  }
+};
 </script>
 
 <template>
