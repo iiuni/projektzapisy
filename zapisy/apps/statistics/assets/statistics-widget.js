b7bfa70761dc67d0356b4a7132c7eb9e0d51d460
mbaugustyn <michal.augustyn@outlook.com>
2024-07-27 21:05:54 +0200

    Refactor statistics

--- zapisy/apps/statistics/assets/statistics-widget.js
+++ zapisy/apps/statistics/assets/statistics-widget.js
@@ -1,26 +1,16 @@
-// import Vue from "vue";
-// import StatisticsList from "./components/StatisticsList.vue";
-// import StatisticsFilter from "./components/StatisticsFilter.vue";
-// import store from "./store";
+import { createApp } from "vue";
+import StatisticsList from "./components/StatisticsList.vue";
+import StatisticsFilter from "./components/StatisticsFilter.vue";
+import store from "./store";
 
-// new Vue({
-//   el: "#statistics-filter",
-//   components: {
-//     StatisticsFilter,
-//   },
-//   render: function (h) {
-//     return h(StatisticsFilter);
-//   },
-//   store,
-// });
+if (document.getElementById("statistics-filter") !== null) {
+  const statisticsFilterApp = createApp(StatisticsFilter);
+  statisticsFilterApp.use(store);
+  statisticsFilterApp.mount("#statistics-filter");
+}
 
-// new Vue({
-//   el: "#statistics-list",
-//   components: {
-//     StatisticsList,
-//   },
-//   render: function (h) {
-//     return h(StatisticsList);
-//   },
-//   store,
-// });
+if (document.getElementById("statistics-list") !== null) {
+  const statisticsListApp = createApp(StatisticsList);
+  statisticsListApp.use(store);
+  statisticsListApp.mount("#statistics-list");
+}
--- zapisy/apps/statistics/assets/store/index.ts
+++ zapisy/apps/statistics/assets/store/index.ts
@@ -1,16 +1,13 @@
-// import Vue from "vue";
-// import Vuex from "vuex";
+import { createStore } from "vuex";
 
-// import courses from "./courses";
-// import filters from "./filters";
-// import sorting from "./sorting";
+import courses from "./courses";
+import filters from "./filters";
+import sorting from "./sorting";
 
-// Vue.use(Vuex);
-
-// export default new Vuex.Store({
-//   modules: {
-//     courses,
-//     filters,
-//     sorting,
-//   },
-// });
+export default createStore({
+  modules: {
+    courses,
+    filters,
+    sorting,
+  },
+});
