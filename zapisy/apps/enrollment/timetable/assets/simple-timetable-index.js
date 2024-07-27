48bec9e958ff4a5914ddeb4c5864a1341e4952d8
mbaugustyn <michal.augustyn@outlook.com>
2024-07-27 21:14:19 +0200

    Refactor simple-time-table index

--- zapisy/apps/enrollment/timetable/assets/simple-timetable-index.js
+++ zapisy/apps/enrollment/timetable/assets/simple-timetable-index.js
@@ -1,40 +1,25 @@
-// // Instantiates timetable component.
-// //
-// // The timetable app assumes that DOM has an element of id #timetable. It also
-// // reads information about the groups that should be displayed from <script
-// // type="application/json"></script> element. The data is expected to be a list
-// // of GroupJSON objects as defined in `models.ts`.
+// Instantiates timetable component.
+//
+// The timetable app assumes that DOM has an element of id #timetable. It also
+// reads information about the groups that should be displayed from <script
+// type="application/json"></script> element. The data is expected to be a list
+// of GroupJSON objects as defined in `models.ts`.
 
-// import Vue from "vue";
-// import SimpleTimetable from "./components/SimpleTimetable.vue";
-// import { Group } from "./models";
+import SimpleTimetable from "./components/SimpleTimetable.vue";
+import { Group } from "./models";
+import { createApp } from "vue";
 
+// Add comment -  This is removed in vue3
 // Vue.config.productionTip = false;
 
-// new Vue({
-//   el: "#timetable",
-//   components: { SimpleTimetable },
-//   data: {
-//     groups: [],
-//   },
-//   render: function (h) {
-//     return h(SimpleTimetable, {
-//       props: {
-//         groups: this.groups,
-//       },
-//     });
-//   },
-//   created: function () {
-//     this.update_groups();
-//   },
-//   methods: {
-//     update_groups: function () {
-//       const groupsDump = JSON.parse(
-//         document.getElementById("timetable-data").innerHTML
-//       );
-//       for (const groupDump of groupsDump) {
-//         this.groups.push(new Group(groupDump));
-//       }
-//     },
-//   },
-// });
+if (
+  document.getElementById("timetable-data") !== null &&
+  document.getElementById("timetable") !== null
+) {
+  const groupsDump = JSON.parse(
+    document.getElementById("timetable-data").innerHTML
+  );
+  const groups = groupsDump.map((groupDump) => new Group(groupDump));
+  const timetableApp = createApp(SimpleTimetable, { groups });
+  timetableApp.mount("#timetable");
+}
