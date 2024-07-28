55b57c092f60530875f4a32cc40670f81c2d0dc2
mbaugustyn <michal.augustyn@outlook.com>
2024-07-28 10:04:56 +0200

    Fix multiselect styles

--- zapisy/apps/enrollment/timetable/assets/components/filters/MultiSelectFilter.vue
+++ zapisy/apps/enrollment/timetable/assets/components/filters/MultiSelectFilter.vue
@@ -203,9 +203,7 @@ watch(
   </div>
 </template>
 
-<style lang="scss" scoped>
-// TODO @import "~vue-multiselect/dist/vue-multiselect.min.css";
-</style>
+<style src="vue-multiselect/dist/vue-multiselect.css"/>
 
 <style lang="scss">
 .multiselect__clear {
