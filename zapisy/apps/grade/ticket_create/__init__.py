5a8523c704afbc13dabd2a6c643e300ced33f898
Paweł <lgpawel@users.noreply.github.com>
2026-09-23 12:51:12 +0200

    Handling some deprecated stuff

--- zapisy/apps/grade/ticket_create/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = "apps.grade.ticket_create.apps.TicketsAppConfig"
--- zapisy/zapisy/settings.py
+++ zapisy/zapisy/settings.py
@@ -354,7 +354,7 @@ DEBUG_TOOLBAR_CONFIG = {
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
         'LOCATION': '127.0.0.1:11211',
         'TIMEOUT': 300,
     }
